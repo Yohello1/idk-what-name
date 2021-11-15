8a42c1f3860d3c9df80a85a05c6e2abb5a536bee
Yohello1 <mp2702737@gmail.com>
2021-11-14 22:17:43 -0500

    I am a stupido, I cannot multi-thread rendering

--- SDL/not_fixed.cpp
+++ SDL/not_fixed.cpp
@@ -6,8 +6,6 @@
 #include <random>
 #include <cmath>
 #include <memory>
-#include <thread>
-#include <mutex>
 #define LOGICAL_WINDOW_WIDTH 256
 #define ACTUAL_WINDOW_WIDTH 1024
 //time
@@ -46,15 +44,37 @@ struct cord_2d
     int y_pos;
 };
 
+pthread_mutex_t locks[LOGICAL_WINDOW_WIDTH][LOGICAL_WINDOW_WIDTH];
 struct position pixels[LOGICAL_WINDOW_WIDTH][LOGICAL_WINDOW_WIDTH];
 struct position new_version[LOGICAL_WINDOW_WIDTH][LOGICAL_WINDOW_WIDTH];
-void scr_dump();
+void scr_dump_state();
+void scr_dump_colour();
+
 void redraw_and_render();
 void excecution_finished();
 // void sand_sim();
 
 // Thread functions
-void polling_thread();
+// void polling_thread();
+// void rendering_thread_function();
+
+/*
+Im sorry threading functions, 
+you were meant to be so much more
+you were meant to save us
+help us
+but, the limitations of SDL
+limit our abilities
+
+ANYWAYS SIMULATION STUFF GO GO GO
+WE HAVE HOSTILES INCOMING
+
+SIR WE CANT HOLD EM
+
+I SAID HOLD EM, WE JUST NEED TO HOLD ON FOR ANOTHER-
+OH ODIN WHAT'S WHAT
+*/
+
 
 // The ratios
 const uint_fast8_t actual_2_logic_ratio = ACTUAL_WINDOW_WIDTH / LOGICAL_WINDOW_WIDTH;
@@ -83,32 +103,18 @@ int main()
     {
         for (int y_pos = 0; y_pos < 0; y_pos++)
         {
-            pixels[x_pos][y_pos].r = pixels[x_pos][y_pos].g = pixels[x_pos][y_pos].b = pixels[x_pos][y_pos].a = pixels[x_pos][y_pos].temperature = 0;
+            pixels[x_pos][y_pos].r = pixels[x_pos][y_pos].g = pixels[x_pos][y_pos].b = pixels[x_pos][y_pos].a = pixels[x_pos][y_pos].temperature = 255;
             pixels[x_pos][y_pos].state_now = empty;
         }
     }
 
-    // idk why I would keep it here but eh
-    // bool quit = false;
-    // int i = 0;
-    // while (!quit)
-    // {
-
-    //     if (event.type == SDL_QUIT)
-    //     {
-    //         quit = true;
-    //         break;
-    //         std::cout << "KILL IT NOW " << std::endl;
-    //     }
-    // }
-
     // Joining the threads so I dont have to put down a revolution
     SDL_RenderPresent(renderer);
 
-    // std::thread render_thread_pointer(rendering_thread, &renderer);
-    std::thread polling_thread_pointer(polling_thread);
+    std::cout << "So the threads have started" << std::endl;
     bool quit = false;
-    int i = 0;
+    uint i = 0;
+    int mouse_x =0, mouse_y = 0;
     while (!quit)
     {
         while (SDL_PollEvent(&event) != 0)
@@ -119,12 +125,43 @@ int main()
                 quit = true;
             }
         }
+        switch (event.button.button)
+        {
+        case SDL_BUTTON_LEFT:
+            SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
+            SDL_GetMouseState(&mouse_x, &mouse_y);
+            std::cout << "(" << mouse_x / actual_2_logic_ratio << "," << mouse_y / actual_2_logic_ratio << ")" << std::endl;
+            SDL_RenderDrawPoint(renderer, mouse_x / actual_2_logic_ratio, mouse_y / actual_2_logic_ratio);
+            SDL_RenderPresent(renderer);
+            pixels[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio].r = pixels[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio].g = pixels[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio].a = 255;
+            pixels[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio].b = 0;
+            pixels[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio].state_now = fixed_pos;
+            SDL_RenderPresent(renderer);
+            break;
+        case SDL_BUTTON_RIGHT:
+            // set draw colour
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+            // Get mouse position
+            SDL_GetMouseState(&mouse_x, &mouse_y);
+            //Output location
+            std::cout << "(" << mouse_x / actual_2_logic_ratio << "," << mouse_y / actual_2_logic_ratio << ")" << std::endl;
+            // Get mouse position, convert to logical position, then make like a block around it which is 8x8 to make I think white
+            for (int y_pos = (mouse_y / actual_2_logic_ratio) - 4; y_pos != LOGICAL_WINDOW_WIDTH - 1 && y_pos < (mouse_y / actual_2_logic_ratio) + 4; y_pos++)
+            {
+                for (int x_pos = (mouse_x / actual_2_logic_ratio) - 4; x_pos != LOGICAL_WINDOW_WIDTH - 1 && x_pos < (mouse_x / actual_2_logic_ratio) + 4; x_pos++)
+                {
+                    // Drawing , outputing position, draw, and seting new state
+                    SDL_RenderDrawPoint(renderer, x_pos, y_pos);
+                    std::cout << "(" << x_pos << "," << y_pos << ")" << std::endl;
+                    pixels[x_pos][y_pos].state_now = solid;
+                    pixels[x_pos][y_pos].r = pixels[x_pos][y_pos].g = pixels[x_pos][y_pos].a = pixels[x_pos][y_pos].b = 255;
+                }
+            }
 
-        i++;
-        std::cout << "Itteration rendering" << i << std::endl;
+            break;
+        }
         for (int x_pos = 0; x_pos < LOGICAL_WINDOW_WIDTH; x_pos++)
         {
-
             for (int y_pos = 0; y_pos < LOGICAL_WINDOW_WIDTH; y_pos++)
             {
                 SDL_SetRenderDrawColor(renderer, pixels[x_pos][y_pos].r, pixels[x_pos][y_pos].g, pixels[x_pos][y_pos].b, pixels[x_pos][y_pos].a);
@@ -133,87 +170,32 @@ int main()
         }
         SDL_RenderPresent(renderer);
     }
-    std::cout << "So the threads have started" << std::endl;
-
-    // render_thread_pointer.join();
-    polling_thread_pointer.join();
 
     // So this is the reason it was being dumped LOL
     // scr_dump();
+    scr_dump_state();
     excecution_finished();
 }
 
-void polling_thread()
-{
-     int mouse_x = 0;
-     int mouse_y = 0;
-
-    bool quit = false;
-    while (!quit)
-    {
-        while (SDL_PollEvent(&event) != 0)
-        {
-            //User requests quit
-            if (event.type == SDL_QUIT)
-            {
-                quit = true;
-            }
-
-            /*
-            For some god foresaken reason this should work,
-            but like I dont think it will work
-            but at this point
-            who even cares
-            */
-            switch (event.button.button)
-            {
-            case SDL_BUTTON_LEFT:
-                SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
-                SDL_GetMouseState(&mouse_x, &mouse_y);
-                std::cout << "(" << mouse_x / actual_2_logic_ratio << "," << mouse_y / actual_2_logic_ratio << ")" << std::endl;
-                SDL_RenderDrawPoint(renderer, mouse_x / actual_2_logic_ratio, mouse_y / actual_2_logic_ratio);
-                SDL_RenderPresent(renderer);
-                pixels[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio].r = pixels[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio].g = pixels[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio].a = 255;
-                pixels[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio].b = 0;
-                pixels[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio].state_now = fixed_pos;
-                SDL_RenderPresent(renderer);
-                break;
-            case SDL_BUTTON_RIGHT:
-                // set draw colour
-                SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-                // Get mouse position
-                SDL_GetMouseState(&mouse_x, &mouse_y);
-                //Output location
-                std::cout << "(" << mouse_x / actual_2_logic_ratio << "," << mouse_y / actual_2_logic_ratio << ")" << std::endl;
-                // Get mouse position, convert to logical position, then make like a block around it which is 8x8 to make I think white
-                for (int y_pos = (mouse_y / actual_2_logic_ratio) - 4; y_pos != LOGICAL_WINDOW_WIDTH - 1 && y_pos < (mouse_y / actual_2_logic_ratio) + 4; y_pos++)
-                {
-                    for (int x_pos = (mouse_x / actual_2_logic_ratio) - 4; x_pos != LOGICAL_WINDOW_WIDTH - 1 && x_pos < (mouse_x / actual_2_logic_ratio) + 4; x_pos++)
-                    {
-                        // Drawing , outputing position, draw, and seting new state
-                        SDL_RenderDrawPoint(renderer, x_pos, y_pos);
-                        std::cout << "(" << x_pos << "," << y_pos << ")" << std::endl;
-                        pixels[x_pos][y_pos].state_now = solid;
-                        pixels[x_pos][y_pos].r = pixels[x_pos][y_pos].g = pixels[x_pos][y_pos].a = pixels[x_pos][y_pos].b = 255;
-                    }
-                    SDL_RenderPresent(renderer);
-                }
-
-                break;
-            }
-        }
-    }
-    std::cout << "Polling thread being killed" << std::endl;
-}
-
 // Debug functions
-void scr_dump()
+void scr_dump_state()
 {
     for (int y_pos = 0; y_pos < LOGICAL_WINDOW_WIDTH; y_pos++)
     {
         for (int x_pos = 0; x_pos < LOGICAL_WINDOW_WIDTH; x_pos++)
         {
             std::cout << pixels[x_pos][y_pos].state_now;
+        }
+        std::cout << std::endl;
+    }
+}
+void scr_dump_colour()
+{
+    for (int y_pos = 0; y_pos < LOGICAL_WINDOW_WIDTH; y_pos++)
+    {
+        for (int x_pos = 0; x_pos < LOGICAL_WINDOW_WIDTH; x_pos++)
+        {
+            std::cout << "(" << pixels[x_pos][y_pos].r << pixels[x_pos][y_pos].g << pixels[x_pos][y_pos].b << ")";
         }
         std::cout << std::endl;
     }
