6c47d7ff7f4e18ee203c62a2d1ba11541f4d2f9e
Yohello1 <mp2702737@gmail.com>
2021-12-20 01:03:34 -0500

    Relearning a lot of old lessons

---
+++ SDL/jeux-video/rand_testing.hpp
@@ -0,0 +1,15 @@
+int count_sand(position pixels[LOGICAL_WINDOW_WIDTH][LOGICAL_WINDOW_WIDTH])
+{
+    int i = 0;
+    for (int x_pos = 0; x_pos < LOGICAL_WINDOW_WIDTH; x_pos++)
+    {
+        for (int y_pos = LOGICAL_WINDOW_WIDTH; y_pos > 0; y_pos--)
+        {
+            if(pixels[x_pos][y_pos].state_now == solid)
+            {
+                i++;
+            }
+        }
+    }
+    return i;
+}
