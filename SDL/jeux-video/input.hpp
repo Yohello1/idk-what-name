a343d9b20cd834b11db8df602cdfc4228b5dbe43
Yohello1 <mp2702737@gmail.com>
2021-12-22 22:35:28 -0500

    So input kinda works, kinky

--- SDL/jeux-video/input.hpp
+++ SDL/jeux-video/input.hpp
@@ -2,41 +2,44 @@
 // This is gonna be painful
 // #include "base_data.hpp"
 #include <SDL2/SDL.h>
-void poll_usr_input(position usr_input[LOGICAL_WINDOW_WIDTH][LOGICAL_WINDOW_WIDTH], SDL_Event *event, bool quit, int actual_2_logic_ratio)
+bool poll_usr_input(bool changed[LOGICAL_WINDOW_WIDTH][LOGICAL_WINDOW_WIDTH], position usr_input[LOGICAL_WINDOW_WIDTH][LOGICAL_WINDOW_WIDTH], SDL_Event *event, bool quit, int actual_2_logic_ratio)
 {
-    if (SDL_PollEvent(event) && event->type == SDL_QUIT)
-    {
-        quit = true;
-    }
+    bool quit_now = false;
     int mouse_x = 0;
     int mouse_y = 0;
-    if ((event->motion.state & SDL_BUTTON_LMASK) == SDL_BUTTON_LEFT)
+    while(SDL_PollEvent(event) != 0)
     {
-        SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
-        SDL_GetMouseState(&mouse_x, &mouse_y);
-        std::cout << "(" << mouse_x / actual_2_logic_ratio << "," << mouse_y / actual_2_logic_ratio << ")" << std::endl;
-        SDL_RenderDrawPoint(renderer, mouse_x / actual_2_logic_ratio, mouse_y / actual_2_logic_ratio);
-        SDL_RenderPresent(renderer);
-        usr_input[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio].r = usr_input[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio].g = usr_input[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio].a = 255;
-        usr_input[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio].b = 0;
-        usr_input[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio].state_now = fixed_pos;
-        SDL_RenderPresent(renderer);
-        std::cout << "Left" << std::endl;
+        // idk why this doesn't
+        if (SDL_PollEvent(event) && event->type == SDL_QUIT)
+        {
+            quit_now = true;
+            std::cout << "SHOULD NOW STOP" << std::endl;
+        }
+
+        else if ((event->motion.state & SDL_BUTTON_LMASK) == SDL_BUTTON_LEFT)
+        {
+            SDL_GetMouseState(&mouse_x, &mouse_y);
+            std::cout << "(" << mouse_x / actual_2_logic_ratio << "," << mouse_y / actual_2_logic_ratio << ")" << std::endl;
+            usr_input[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio].r = usr_input[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio].g = usr_input[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio].a = 255;
+            usr_input[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio].b = 0;
+            usr_input[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio].state_now = solid;
+            changed[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio] = true;
+            std::cout << "Left" << std::endl;
+        }
+
+        else if ((event->motion.state & SDL_BUTTON_RMASK) == SDL_BUTTON_RIGHT)
+        {
+            SDL_GetMouseState(&mouse_x, &mouse_y);
+            std::cout << "(" << mouse_x / actual_2_logic_ratio << "," << mouse_y / actual_2_logic_ratio << ")" << std::endl;
+            usr_input[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio].r = usr_input[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio].g = usr_input[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio].a = 255;
+            usr_input[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio].b = 0;
+            usr_input[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio].state_now = solid;
+            changed[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio] = true;
+            std::cout << "Right" << std::endl;
+        }
     }
 
-    if ((event->motion.state & SDL_BUTTON_RMASK) == SDL_BUTTON_RIGHT)
-    {
-        SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
-        SDL_GetMouseState(&mouse_x, &mouse_y);
-        std::cout << "(" << mouse_x / actual_2_logic_ratio << "," << mouse_y / actual_2_logic_ratio << ")" << std::endl;
-        SDL_RenderDrawPoint(renderer, mouse_x / actual_2_logic_ratio, mouse_y / actual_2_logic_ratio);
-        SDL_RenderPresent(renderer);
-        usr_input[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio].r = usr_input[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio].g = usr_input[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio].a = 255;
-        usr_input[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio].b = 0;
-        usr_input[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio].state_now = fixed_pos;
-        SDL_RenderPresent(renderer);
-        std::cout << "Right" << std::endl;
-    }
+    return quit_now;
 }
 
 // Gonna throw this part onto the backburner for now whilst I figure out what the heck is going on
@@ -60,7 +63,9 @@ void mix_new_version_usr_input(bool changed[LOGICAL_WINDOW_WIDTH][LOGICAL_WINDOW
 
                 usr_input[x_pos][y_pos].r = usr_input[x_pos][y_pos].g = usr_input[x_pos][y_pos].b = usr_input[x_pos][y_pos].a = usr_input[x_pos][y_pos].temperature = usr_input[x_pos][y_pos].pressure = 0;
                 usr_input[x_pos][y_pos].state_now = empty;
+                changed[x_pos][y_pos] = false;
             }
+            
         }
     }
 }
