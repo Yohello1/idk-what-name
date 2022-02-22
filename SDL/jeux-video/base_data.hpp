7e967ded46a6a1fe75a2944504d63fbd12928631
Yohello1 <yohello@yohello-deb.localdomain>
2022-02-21 23:41:20 -0500

    Code monkey broke this

--- SDL/jeux-video/base_data.hpp
+++ SDL/jeux-video/base_data.hpp
@@ -1,33 +1,30 @@
-#include <SDL2/SDL.h>
-#include <random>
 #define LOGICAL_WINDOW_WIDTH 256
 #define ACTUAL_WINDOW_WIDTH 1024
 typedef std::chrono::high_resolution_clock Clock;
 std::atomic<bool> quit_now;
-#define STB_TRUETYPE_IMPLEMENTATION
-//time
+// time
 unsigned int current_time = (unsigned int)time(NULL);
 // pointers to these things
 SDL_Renderer *renderer;
 SDL_Window *window;
 SDL_Event event;
 
-// something needs to bedone about this  
+// something needs to bedone about this
 // Use density, and type (static or dynamic)
 enum pixel_state
 {
     empty,
-	fluid,
+    fluid,
     solid,
     fixed_pos
 };
 struct position
 {
     pixel_state state_now;
-	// Yes I could do all these vars in one line, but this is "cleaner"
-    uint8_t  r,g,b,a;
-	uint8_t density; 
-	uint8_t pressure;
+    // Yes I could do all these vars in one line, but this is "cleaner"
+    uint8_t r, g, b, a;
+    uint8_t density;
+    uint8_t pressure;
     uint16_t temperature;
 };
 
@@ -39,7 +36,6 @@ struct cord_2d
 
 struct colour
 {
-    uint8_t r = 255,g = 255,b = 255,a = 0;
-}; 
-
+    uint8_t r = 255, g = 255, b = 255, a = 0;
+};
 
--- SDL/jeux-video/ui/ui.hpp
+++ SDL/jeux-video/ui/ui.hpp
@@ -1,4 +1,9 @@
 // #include <..base_data.hpp>
+#include "stb_truetype.h"
+#define STB_TRUETYPE_IMPLEMENTATION
+char buffer[24 << 20];
+stbtt_fontinfo font;
+int i, j, ascent, ch = 0;
 
 /*
 Oi listen up
@@ -20,43 +25,11 @@ namespace ui
         // I am not smort
     }
 
-    // Cant I just copy this fron sandy thing but with a diff set of stuff???
-    // void draw_box(cord_2d start, cord_2d end, colour colours, colour user_interface[LOGICAL_WINDOW_WIDTH][LOGICAL_WINDOW_WIDTH])
-    // {
-    //     // The location is relative to where it is on the screen/window :P
-    //     // That made no sense
-    //     int higher_x = std::min(start.x_pos, end.x_pos);
-    //     int higher_y = std::min(start.y_pos, end.y_pos);
-    //     int lower_x = std::max(start.x_pos, end.y_pos);
-    //     int lower_y = std::max(start.y_pos, end.y_pos);
-
-    //     for (int x_pos = higher_x; x_pos < lower_x; x_pos++)
-    //     {
-    //         for (int y_pos = higher_y; y_pos < lower_y; y_pos++)
-    //         {
-    //             user_interface[x_pos][y_pos].r = colours.r;
-    //             user_interface[x_pos][y_pos].g = colours.g;
-    //             user_interface[x_pos][y_pos].b = colours.b;
-    //             user_interface[x_pos][y_pos].a = colours.a;
-    //             // std::cout << "Hello world" << '\n';
-    //         }
-    //     }
-    // }
-    // not even legacy
-    // void merge_render_and_user_interface(colour user_interface[LOGICAL_WINDOW_WIDTH][LOGICAL_WINDOW_WIDTH], position render[LOGICAL_WINDOW_WIDTH][LOGICAL_WINDOW_WIDTH])
-    // {
-    //     for (int x_pos = 0; x_pos < LOGICAL_WINDOW_WIDTH; x_pos++)
-    //     {
-    //         for (int y_pos = 0; y_pos < LOGICAL_WINDOW_WIDTH; y_pos++)
-    //         {
-    //             render[x_pos][y_pos].r = (user_interface[x_pos][y_pos].r * user_interface[x_pos][y_pos].a + render[x_pos][y_pos].r) / (1 + (user_interface[x_pos][y_pos].a / 255));
-    //             render[x_pos][y_pos].g = (user_interface[x_pos][y_p  os].g * user_interface[x_pos][y_pos].a + render[x_pos][y_pos].g) / (1 + (user_interface[x_pos][y_pos].a / 255));
-    //             render[x_pos][y_pos].b = (user_interface[x_pos][y_pos].b * user_interface[x_pos][y_pos].a + render[x_pos][y_pos].b) / (1 + (user_interface[x_pos][y_pos].a / 255));
-    //             render[x_pos][y_pos].a = 255;
-    //             // std::cout << "hi" << '\n';
-    //         }
-    //     }
-    // }
+    void init_text(const char *font_path)
+    {
+        // fread(buffer, 1, 1000000, fopen(font_path, "rb"));
+        // stbtt_InitFont(&font, buffer, 0);
+    }
 
     class boxes
     {
@@ -106,6 +79,8 @@ namespace ui
         // bool perm = false;
         colour box_colour;
 
+        int baseline;
+        float scale, xpos = 2;
         std::vector<char> text_to_render;
 
         int higher_x = 0;
@@ -114,7 +89,7 @@ namespace ui
         int lower_y = 0;
 
     public:
-        text(cord_2d cord_1, cord_2d cord_2, colour colour_new, string text_to_render_new)
+        text(cord_2d cord_1, cord_2d cord_2, colour colour_new, std::string text_to_render_new)
         {
             std::copy(text_to_render_new.begin(), text_to_render_new.end(), std::back_inserter(text_to_render));
             // perm = perm_new;
@@ -127,8 +102,6 @@ namespace ui
 
         void draw(position render[LOGICAL_WINDOW_WIDTH][LOGICAL_WINDOW_WIDTH])
         {
-            
-
         }
         void change_colour(colour colour_new)
         {
