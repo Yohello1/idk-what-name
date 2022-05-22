a999a88ec88a1a4babfe630316681bfcb492965c
Yohello1 <mp2702737@gmail.com>
2022-05-22 02:15:34 -0400

    Borroweda bagguette

--- SDL/jeux-video/start/base_data.hpp
+++ SDL/jeux-video/start/base_data.hpp
@@ -1,5 +1,26 @@
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <SDL2/SDL.h>
+#include <ctime>
+#include <iostream>
+#include <random>
+#include <cmath>
+#include <chrono>
+#include <mutex>
+#include <thread>
+#include <atomic>
+#include <string>
+#include <memory>
+#include <bitset>
+#include <queue>
+#include <unordered_map>
+#include <set>
+#define STB_TRUETYPE_IMPLEMENTATION
+#define BLEND_TEXT 0
 #define LOGICAL_WINDOW_WIDTH 256
 #define ACTUAL_WINDOW_WIDTH 1024
+
 typedef std::chrono::high_resolution_clock Clock;
 std::atomic<bool> quit_now;
 // time
@@ -11,13 +32,13 @@ SDL_Event event;
 
 // something needs to bedone about this
 // Use density, and type (static or dynamic)
+// entity is rlly just fixed_pos with extra steps
 enum pixel_state
 {
     empty,
     fluid,
     solid,
-    fixed_pos,
-    enity
+    fixed_pos
 };
 struct position
 {
@@ -31,11 +52,47 @@ struct position
 
 struct cord_2d
 {
-    int x_pos;
-    int y_pos;
+    u_int16_t x_pos;
+    u_int16_t y_pos;
 };
 
 struct colour
 {
     uint8_t r, g, b, a;
 };
+
+
+
+namespace init
+{
+    void start()
+    {
+        SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+        // Printing the time
+        std::cout << "Time = " << current_time << "\n";
+        // setting random seed
+        srand(current_time);
+        // rise my glorious creation*
+
+        SDL_Init(SDL_INIT_VIDEO);
+        SDL_CreateWindowAndRenderer(ACTUAL_WINDOW_WIDTH, ACTUAL_WINDOW_WIDTH, 0, &window, &renderer);
+        SDL_SetWindowTitle(window, "Gamen't");
+        SDL_RenderSetLogicalSize(renderer, LOGICAL_WINDOW_WIDTH, LOGICAL_WINDOW_WIDTH);
+        SDL_RenderClear(renderer);
+    }
+
+    void array_clean_start(position pixels[LOGICAL_WINDOW_WIDTH][LOGICAL_WINDOW_WIDTH])
+    {
+        for (int x_pos = 0; x_pos < LOGICAL_WINDOW_WIDTH; x_pos++)
+        {
+            for (int y_pos = 0; y_pos < LOGICAL_WINDOW_WIDTH; y_pos++)
+            {
+                pixels[x_pos][y_pos].a = pixels[x_pos][y_pos].g = pixels[x_pos][y_pos].b = pixels[x_pos][y_pos].r = 0;
+                pixels[x_pos][y_pos].state_now = empty;
+                pixels[x_pos][y_pos].temperature = 0;
+                pixels[x_pos][y_pos].pressure = 0;
+                pixels[x_pos][y_pos].density = 0;
+            }
+        }
+    }
+}
