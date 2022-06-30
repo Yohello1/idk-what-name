e887e64d867186ae77c342886c0efe1ba3fe09d7
Yohello1 <mp2702737@gmail.com>
2022-06-29 20:00:57 -0400

    The ediful complex

---
+++ SDL/opengl_migration/CMakeFiles/tri.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/media/yohello/hrm2/code-stuff/generation/SDL/opengl_migration/glad.c" "CMakeFiles/tri.dir/glad.c.o" "gcc" "CMakeFiles/tri.dir/glad.c.o.d"
+  "/media/yohello/hrm2/code-stuff/generation/SDL/opengl_migration/main.cpp" "CMakeFiles/tri.dir/main.cpp.o" "gcc" "CMakeFiles/tri.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ SDL/opengl_migration/CMakeFiles/tri.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for tri.
+# This may be replaced when dependencies are built.
