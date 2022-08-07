49e5336a057eab0d1d48daa33265a168ad55a0df
Yohello1 <mp2702737@gmail.com>
2022-08-07 03:48:59 -0400

    It has evolved into stage 3 cancer

--- SDL/jeux-video/debug/debug.hpp
+++ SDL/jeux-video/debug/debug.hpp
@@ -26,18 +26,18 @@ namespace Debug
                 {
                     for (int y_pos = y_chunk * 8; y_pos > (y_chunk * 8 - 8); y_pos--)
                     {
-                        for (int x_pos = (x_chunk * 8 - 8); x_pos < x_chunk * 8; x_pos++)
+                        for (int x_pos = x_chunk * 8; x_pos < x_chunk * 8 + 8; x_pos++)
                         {
                             ui[x_pos][y_pos].r = 50;
-
                         }
                     }
-
                 }
-                std::cout << chunk_temp[x_chunk][y_chunk] << ',';
+                // std::cout << chunk_temp[x_chunk][y_chunk] /*<< ','*/;
             }
-            std::cout << '\n';
+            // std::cout << '\n';
         }
+        // std::cout << '\n';
+        // std::cout << '\n';
     }
 
 }
