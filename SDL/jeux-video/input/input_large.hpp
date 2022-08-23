43faf0113cb6c0cf4e26093c0572d49b86c1c541
Yohello1 <mp2702737@gmail.com>
2022-08-23 05:20:23 -0400

    what happened to them all

--- SDL/jeux-video/input/input_large.hpp
+++ SDL/jeux-video/input/input_large.hpp
@@ -1,7 +1,6 @@
 // Polling for user input
 // This is gonna be painful
 // #include "base_data.hpp"
-#include <SDL2/SDL.h>
 
 // keep track of if it's held down or not
 namespace Input_Large
@@ -105,9 +104,23 @@ namespace Input_Large
                 std::cout << "(" << mouse_x / actual_2_logic_ratio << "," << mouse_y / actual_2_logic_ratio << ")" << '\n';
                 #endif
 
-                // Figure 3
-                // Convert to logical units, and figure out what needs to be done
-                // get the mid, div the size by two, then that is the distance in each direction it fills out
+                usr_input[mouse_x/actual_2_logic_ratio][mouse_y/actual_2_logic_ratio].r = 255;
+                usr_input[mouse_x/actual_2_logic_ratio][mouse_y/actual_2_logic_ratio].g = 255;
+                usr_input[mouse_x/actual_2_logic_ratio][mouse_y/actual_2_logic_ratio].b = 255;
+                usr_input[mouse_x/actual_2_logic_ratio][mouse_y/actual_2_logic_ratio].a = 255;
+                usr_input[mouse_x/actual_2_logic_ratio][mouse_y/actual_2_logic_ratio].temperature = 0;
+                usr_input[mouse_x/actual_2_logic_ratio][mouse_y/actual_2_logic_ratio].pressure = 0;
+                  changed[mouse_x/actual_2_logic_ratio][mouse_y/actual_2_logic_ratio] = true;
+
+            }
+            else if(left_down == true && shift == true)
+            {
+                recheck = true;
+                std::cout << recheck << "Status\n";
+                // Fetch mouse state, or cords
+                SDL_GetMouseState(&mouse_x, &mouse_y);
+
+
                 for (int y_pos = (mouse_y / actual_2_logic_ratio) - 4;
                      y_pos != LOGICAL_WINDOW_WIDTH - 1 &&
                      y_pos < (mouse_y / actual_2_logic_ratio) + 4;
@@ -124,103 +137,14 @@ namespace Input_Large
                         usr_input[x_pos][y_pos].b = 222;
                         usr_input[x_pos][y_pos].a = 255;
                         usr_input[x_pos][y_pos].pressure = 50;
-                        usr_input[x_pos][y_pos].flow = 0;
+                        usr_input[x_pos][y_pos].flow = 4;
+                        usr_input[x_pos][y_pos].inert = false;
                         // Tells us later on which one changed
                         changed[x_pos][y_pos] = true;
                     }
                 }
             }
-            // Go to "Figure 1", or 3
-            else if (right_down == true && shift == false)
-            {
-                SDL_GetMouseState(&mouse_x, &mouse_y);
-                #ifdef INPUT_DEBUG
-                std::cout << "(" << mouse_x / actual_2_logic_ratio << "," << mouse_y / actual_2_logic_ratio << ")" << '\n';
-                #endif
-                for (int y_pos = (mouse_y / actual_2_logic_ratio) - 4;
-                     y_pos != LOGICAL_WINDOW_WIDTH - 1 &&
-                     y_pos < (mouse_y / actual_2_logic_ratio) + 4;
-                     y_pos++)
-                {
-                    for (int x_pos = (mouse_x / actual_2_logic_ratio) - 4;
-                         x_pos != LOGICAL_WINDOW_WIDTH - 1 &&
-                         x_pos < (mouse_x / actual_2_logic_ratio) + 4;
-                         x_pos++)
-                    {
-                        usr_input[x_pos][y_pos].r = 153;
-                        usr_input[x_pos][y_pos].g = 80;
-                        usr_input[x_pos][y_pos].b = 0;
-                        usr_input[x_pos][y_pos].a = 255;
-                        usr_input[x_pos][y_pos].flow = 0;
-                        usr_input[x_pos][y_pos].pressure = 10;
-                        changed[x_pos][y_pos] = true;
-                    }
-                }
-                // std::cout << "Right" << '\n';
-            }
-            // Go to "Figure 3"
-            else if (right_down == true && shift == true)
-            {
-                SDL_GetMouseState(&mouse_x, &mouse_y);
-                #ifdef INPUT_DEBUG
-                std::cout << "(" << mouse_x / actual_2_logic_ratio << "," << mouse_y / actual_2_logic_ratio << ")" << '\n';
-                #endif
-                for (int y_pos = (mouse_y / actual_2_logic_ratio) - 4;
-                     y_pos != LOGICAL_WINDOW_WIDTH - 1 &&
-                     y_pos < (mouse_y / actual_2_logic_ratio) + 4;
-                     y_pos++)
-                {
-                    for (int x_pos = (mouse_x / actual_2_logic_ratio) - 4;
-                         x_pos != LOGICAL_WINDOW_WIDTH - 1 &&
-                         x_pos < (mouse_x / actual_2_logic_ratio) + 4;
-                         x_pos++)
-                    {
-                        usr_input[x_pos][y_pos].r = 241;
-                        usr_input[x_pos][y_pos].g = 27;
-                        usr_input[x_pos][y_pos].b = 21;
-                        usr_input[x_pos][y_pos].a = 255;
-                        usr_input[x_pos][y_pos].flow = 0;
-                        usr_input[x_pos][y_pos].inert = true;
-                        usr_input[x_pos][y_pos].pressure = 255;
-                        changed[x_pos][y_pos] = true;
-                    }
-                }
-            }
-            // Go to "Figure 1", or 3
-
-            else if (left_down == true && shift == true)
-            {
-                SDL_GetMouseState(&mouse_x, &mouse_y);
-                #ifdef INPUT_DEBUG
-                std::cout << "(" << mouse_x / actual_2_logic_ratio << "," << mouse_y / actual_2_logic_ratio << ")" << '\n';
-                #endif
-                usr_input[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio].r = 190;
-                usr_input[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio].g = 238;
-                usr_input[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio].b = 254;
-                usr_input[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio].a = 255;
-                usr_input[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio].flow = fluid;
-                changed[mouse_x / actual_2_logic_ratio][mouse_y / actual_2_logic_ratio] = true;
-
-                for (int y_pos = (mouse_y / actual_2_logic_ratio) - 4;
-                     y_pos != LOGICAL_WINDOW_WIDTH - 1 &&
-                     y_pos < (mouse_y / actual_2_logic_ratio) + 4;
-                     y_pos++)
-                {
-                    for (int x_pos = (mouse_x / actual_2_logic_ratio) - 4;
-                         x_pos != LOGICAL_WINDOW_WIDTH - 1 &&
-                         x_pos < (mouse_x / actual_2_logic_ratio) + 4;
-                         x_pos++)
-                    {
-                        usr_input[x_pos][y_pos].r = 190;
-                        usr_input[x_pos][y_pos].g = 238;
-                        usr_input[x_pos][y_pos].b = 254;
-                        usr_input[x_pos][y_pos].a = 255;
-                        usr_input[x_pos][y_pos].flow = 1;
-                        usr_input[x_pos][y_pos].pressure = 20;
-                        changed[x_pos][y_pos] = true;
-                    }
-                }
-            }
+            
         }
 
         // i gotta change this
