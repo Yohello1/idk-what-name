247e0255d962da3f70673aa68a5c05babf45dc86
Yohello1 <mp2702737@gmail.com>
2022-12-22 01:10:20 -0500

    IM A FRAUD

--- SDL/jeux-video/main.cpp
+++ SDL/jeux-video/main.cpp
@@ -30,7 +30,6 @@ std::mutex mtx2;
 entites::Coordinator Conductor;
 const int actual_2_logic_ratio = ACTUAL_WINDOW_WIDTH / LOGICAL_WINDOW_WIDTH;
 
-
 int main()
 {
     std::cout << "Starting" << '\n';
@@ -38,26 +37,107 @@ int main()
     // Starting stuff
     srand(current_time);
 
+    // AAAAAa
+    // https://youtu.be/45MIykWJ-C4
+    glfwInit();
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+    GLfloat vertices[] =
+        {
+            -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,    // Lower left corner
+            0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,     // Lower right corner
+            0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f,  // Upper corner
+            -0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner left
+            0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,  // Inner right
+            0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f      // Inner down
+        };
 
-    // init::start();
+    GLuint indices[] =
+        {
+            0, 3, 5,
+            3, 2, 4,
+            5, 4, 1};
+
+    GLFWwindow *window = glfwCreateWindow(ACTUAL_WINDOW_WIDTH, ACTUAL_WINDOW_HEIGH, "idk", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Could not create window (GLFW)" << '\n';
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    gladLoadGL();
+    glViewport(0, 0, ACTUAL_WINDOW_WIDTH, ACTUAL_WINDOW_HEIGH);
+
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &rendering::vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    GLuint fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragShader, 1, &rendering::fragmentShaderSource, NULL);
+    glCompileShader(fragShader);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragShader);
+
+    GLuint VAO, VBO, EBO;
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    // glGenVertexArrays(1, &VAO);
+    // glGenBuffers(1, &VAO);
+    // glBindVertexArray(VAO);
+    // glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    // glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+
+    glBindVertexArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
 
     array_clean_start(pixels);
-
     std::thread physics(physics::simulate, pixels, new_version);
 
-    // For now
-    // while(!quit_now)
-    while (!glfwWindowShouldClose(w))
+    while (!glfwWindowShouldClose(window))
     {
-
+        glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
     }
     quit_now = true;
     physics.join();
     std::cout << "ENDED" << '\n';
     excecution_finished();
 
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteProgram(shaderProgram);
+    glfwDestroyWindow(window);
+    glfwTerminate();
 }
 
 void excecution_finished(void)
