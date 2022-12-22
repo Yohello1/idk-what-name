04d686b2a5bacaa97d5c82666c37d2c34aa05794
Yohello1 <mp2702737@gmail.com>
2022-12-22 01:10:20 -0500

    IM A FRAUD

--- SDL/jeux-video/render/render.hpp
+++ SDL/jeux-video/render/render.hpp
@@ -1,6 +1,25 @@
 namespace rendering
 {
 
+// Vertex Shader source code
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+//Fragment Shader source code
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"}\n\0";
+
+
+
+
+
     // printShaderInfoLog
     // From OpenGL Shading Language 3rd Edition, p215-216
     // Display (hopefully) useful error messages if shader fails to compile
