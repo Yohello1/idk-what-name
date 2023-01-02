8560858221b1cb43096e78a18be2c228ffbdb920
Yohello1 <mp2702737@gmail.com>
2023-01-02 05:09:49 -0500

    Do I really need help at this point? No, I need smth bigger

--- SDL/jeux-video/main.cpp
+++ SDL/jeux-video/main.cpp
@@ -1,14 +1,8 @@
 #include "start/base_data.hpp"
 #include "render/stb_truetype.h"
 #include "math/vector_math.hpp"
-#include "render/shader_class.h"
-#include "render/VAO.h"
-#include "render/VBO.h"
-#include "render/EBO.h"
-// #include "physics/physics.hpp"
 #include "physics/physics2.hpp"
-// #include "render/render.hpp"
-// #include "input/input_large.hpp"
+#include "render/render.hpp"
 #include "generation/terain.hpp"
 #include "render/ui.hpp"
 #include "physics/entity.hpp"
@@ -38,20 +32,30 @@ entites::Coordinator Conductor;
 // Vertices coordinates
 GLfloat vertices[] =
 {
-	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
-	0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
-	0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // Upper corner
-	-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner left
-	0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner right
-	0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f // Inner down
+	// -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
+	// 0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
+	// 0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // Upper corner
+	// -0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner left
+	// 0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner right
+	// 0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f // Inner down
+
+
+    -1.0f,1.0f,0.0f, // Top left corner
+    1.0f,-1.0f,0.0f, // right bottom corner
+    1.0f,1.0f,0.0f, // top right corner
+
+    -1.0f,-1.0f, 0.0f, // bottom left
+
 };
 
 // Indices for vertices order
 GLuint indices[] =
 {
-	0, 3, 5, // Lower left triangle
-	3, 2, 4, // Lower right triangle
-	5, 4, 1 // Upper triangle
+	// 0, 3, 5, // Lower left triangle
+	// 3, 2, 4, // Upper
+	// 5, 4, 1 // Lower right triangle
+    0,1,2,
+    0,3,1
 };
 
 
@@ -84,6 +88,16 @@ int main()
 
     // Generates Shader object using shaders defualt.vert and default.frag
     Shader shaderProgram("render/shaders/test_vs.glsl", "render/shaders/test_fs.glsl");
+	shaderProgram.Activate();
+    float aspect = (float)ACTUAL_WINDOW_WIDTH/ACTUAL_WINDOW_HEIGH;
+    float half_heigh = ACTUAL_WINDOW_HEIGH/2.f; //ortho size
+    float half_width = half_heigh * aspect;
+    shaderProgram.setMat4("uProjectionMatrix", glm::ortho(-half_width, half_width, -half_heigh, half_heigh, ortho_near, ortho_farr));
+    glm::mat4 view_matrix(1);
+    shaderProgram.setMat4("uViewMatrix", view_matrix);
+
+    std::cout << "Shaders r not ready " << '\n';
+
 
     // Generates Vertex Array Object and binds it
     VAO VAO1;
@@ -108,13 +122,20 @@ int main()
 
     while (!glfwWindowShouldClose(window))
     {
-        glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        shaderProgram.Activate();
-        // glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
-        glfwSwapBuffers(window);
-        glfwPollEvents();
+        glm::ortho(0.0f, 800.0f, 0.0f, 600.0f, 0.1f, 100.0f);
+
+ 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		// Clean the back buffer and assign the new color to it
+		glClear(GL_COLOR_BUFFER_BIT);
+		// Tell OpenGL which Shader Program we want to use
+		// Bind the VAO so OpenGL knows to use it
+		VAO1.Bind();
+		// Draw primitives, number of indices, datatype of indices, index of indices
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+		// Swap the back buffer with the front buffer
+		glfwSwapBuffers(window);
+		// Take care of all GLFW events
+		glfwPollEvents();
     }
     quit_now = true;
     // physics.join();
--- SDL/jeux-video/render/shaders/test_fs.glsl
+++ SDL/jeux-video/render/shaders/test_fs.glsl
@@ -1,6 +1,21 @@
-#version 330 core
-out vec4 FragColor;
+#version 430
+out vec4 fragColor;
+uniform vec3 uColor;
+
 void main()
 {
-   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);
+    // gl_FragCoord contains the window relative coordinate for the fragment.
+    // we use gl_FragCoord.x position to control the red color value.
+    // we use gl_FragCoord.y position to control the green color value.
+    // please note that all r, g, b, a values are between 0 and 1.
+
+    // float windowWidth = 1024.0;
+    // float windowHeight = 1024.0;
+
+    // float r = gl_FragCoord.x / windowWidth;
+    // float g = gl_FragCoord.y / windowHeight;
+    // float b = 1.0;
+    // float a = 1.0;
+    // outputColor = vec4(r, g, b, a);
+    fragColor = vec4(uColor, 1.0);
 }
