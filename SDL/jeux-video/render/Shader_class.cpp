f661ae0b693b3444c0b375b1344b9f0e43b8eeaf
Yohello1 <mp2702737@gmail.com>
2023-01-04 01:04:34 -0500

    I HAVE EYES

--- SDL/jeux-video/render/Shader_class.cpp
+++ SDL/jeux-video/render/Shader_class.cpp
@@ -1,6 +1,6 @@
 #include "shader_class.h"
 
-std::string get_file_contents(const char* filename)
+std::string get_file_contents(const char *filename)
 {
 	std::ifstream in(filename, std::ios::binary);
 	if (in)
@@ -11,84 +11,92 @@ std::string get_file_contents(const char* filename)
 		in.seekg(0, std::ios::beg);
 		in.read(&contents[0], contents.size());
 		in.close();
-		return(contents);
+		return (contents);
 	}
 	throw(errno);
 }
 
-
-Shader::Shader(const char* vertexFile, const char* FragFile)
+Shader::Shader(const char *vertexFile, const char *FragFile)
 {
-    std::string vertexCode = get_file_contents(vertexFile);
-    std::string fragCode = get_file_contents(FragFile);
+	std::string vertexCode = get_file_contents(vertexFile);
+	std::string fragCode = get_file_contents(FragFile);
 
-    const char* vertexSource = vertexCode.c_str();
-    const char* fragSource = fragCode.c_str();
+	const char *vertexSource = vertexCode.c_str();
+	const char *fragSource = fragCode.c_str();
 
 	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexSource, NULL);
 	glCompileShader(vertexShader);
-    compileErrors(vertexShader, "VERTEX");
+	compileErrors(vertexShader, "VERTEX");
 
 	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragSource, NULL);
 	glCompileShader(fragmentShader);
-    compileErrors(fragmentShader, "FRAGMENT");
+	compileErrors(fragmentShader, "FRAGMENT");
 
-    ID = glCreateProgram();
-    glAttachShader(ID, vertexShader);
-    glAttachShader(ID, fragmentShader);
-    glLinkProgram(ID);
+	ID = glCreateProgram();
+	glAttachShader(ID, vertexShader);
+	glAttachShader(ID, fragmentShader);
+	glLinkProgram(ID);
 
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
 }
 
 void Shader::Activate()
 {
-    glUseProgram(ID);
+	glUseProgram(ID);
 }
 
 void Shader::Delete()
 {
-    glDeleteProgram(ID);
+	glDeleteProgram(ID);
 }
 
+void Shader::setInt(const char *name, const GLint &value)
+{
 
-void Shader::setInt(const char* name, const GLint& value) {
-  loc = getUniformID(name);
-  glUniform1iv(loc, 1, &value);
+	loc = getUniformID(name);
+	glUniform1iv(loc, 1, &value);
 }
-void Shader::setUint(const char* name, const GLuint& value) {
-  loc = getUniformID(name);
-  glUniform1uiv(loc, 1, &value);
+void Shader::setUint(const char *name, const GLuint &value)
+{
+	loc = getUniformID(name);
+	glUniform1uiv(loc, 1, &value);
 }
-void Shader::setFloat(const char* name, const GLfloat& value) {
-  loc = getUniformID(name);
-  glUniform1fv(loc, 1, &value);
+void Shader::setFloat(const char *name, const GLfloat &value)
+{
+	loc = getUniformID(name);
+	glUniform1fv(loc, 1, &value);
 }
-void Shader::setVec2(const char* name, const glm::vec2& value) {
-  loc = getUniformID(name);
-  glUniform2fv(loc, 1, &value[0]);
+void Shader::setVec2(const char *name, const glm::vec2 &value)
+{
+	loc = getUniformID(name);
+	glUniform2fv(loc, 1, &value[0]);
 }
-void Shader::setVec3(const char* name, const glm::vec3& value) {
-  loc = getUniformID(name);
-  glUniform3fv(loc, 1, &value[0]);
+void Shader::setVec3(const char *name, const glm::vec3 &value)
+{
+	loc = getUniformID(name);
+	glUniform3fv(loc, 1, &value[0]);
 }
-void Shader::setVec4(const char* name, const glm::vec4& value) {
-  loc = getUniformID(name);
-  glUniform4fv(loc, 1, &value[0]);
+void Shader::setVec4(const char *name, const glm::vec4 &value)
+{
+	loc = getUniformID(name);
+	glUniform4fv(loc, 1, &value[0]);
 }
-void Shader::setMat3(const char* name, const glm::mat3& value) {
-  loc = getUniformID(name);
-  glUniformMatrix3fv(loc, 1, GL_FALSE, &value[0][0]);
+void Shader::setMat3(const char *name, const glm::mat3 &value)
+{
+	loc = getUniformID(name);
+	glUniformMatrix3fv(loc, 1, GL_FALSE, &value[0][0]);
 }
-void Shader::setMat4(const char* name, const glm::mat4& value) {
-  loc = getUniformID(name);
-  glUniformMatrix4fv(loc, 1, GL_FALSE, &value[0][0]);
+void Shader::setMat4(const char *name, const glm::mat4 &value)
+{
+	std::cout << name << '\n';
+	loc = getUniformID(name);
+	glUniformMatrix4fv(loc, 1, GL_FALSE, &value[0][0]);
 }
 
-void Shader::compileErrors(unsigned int shader, const char* type)
+void Shader::compileErrors(unsigned int shader, const char *type)
 {
 	// Stores status of compilation
 	GLint hasCompiled;
@@ -100,7 +108,8 @@ void Shader::compileErrors(unsigned int shader, const char* type)
 		if (hasCompiled == GL_FALSE)
 		{
 			glGetShaderInfoLog(shader, 1024, NULL, infoLog);
-			std::cout << "SHADER_COMPILATION_ERROR for:" << type << "\n" << infoLog << std::endl;
+			std::cout << "SHADER_COMPILATION_ERROR for:" << type << "\n"
+					  << infoLog << std::endl;
 		}
 	}
 	else
@@ -109,24 +118,30 @@ void Shader::compileErrors(unsigned int shader, const char* type)
 		if (hasCompiled == GL_FALSE)
 		{
 			glGetProgramInfoLog(shader, 1024, NULL, infoLog);
-			std::cout << "SHADER_LINKING_ERROR for:" << type << "\n" << infoLog << std::endl;
+			std::cout << "SHADER_LINKING_ERROR for:" << type << "\n"
+					  << infoLog << std::endl;
 		}
 	}
 
-
-
 	/* pfffffft who needs to check for errors
-        Like who needs fake friends?
-        It hurts to cut them off, but it needs to be done
-        Lest they hurt you more
-        */
+		Like who needs fake friends?
+		It hurts to cut them off, but it needs to be done
+		Lest they hurt you more
+		*/
 }
 
-  GLint Shader::getUniformID(const char* name) {
-    loc = glGetUniformLocation(ID, name);
-    if (loc >= 0)
-      return loc;
-    else
-      throw("invalid uniform name");
-    return loc;
-  }
+GLint Shader::getUniformID(const char *name)
+{
+	std::cout << "ID: " << ID << "NAME: " << name << '\n';
+	loc = glGetUniformLocation(ID, name);
+	if (loc >= 0)
+	{
+		return loc;
+	}
+	else
+	{
+		std::cout << "loc too small " << '\n';
+		throw("invalid uniform name");
+	}
+	return loc;
+}
