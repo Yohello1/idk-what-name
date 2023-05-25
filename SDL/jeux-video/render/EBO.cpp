bda3a6b19c3c3ae134b36432ca654960a215e8e4
Yohello1 <mp2702737@gmail.com>
2023-05-25 08:28:24 -0400

    Idk what to do this math class

--- SDL/jeux-video/render/EBO.cpp
+++ SDL/jeux-video/render/EBO.cpp
@@ -2,21 +2,23 @@
 
 class
 {
-  private:
-  public:
-    EBO(GLuint* indices, GLsizeiprt size)
+private:
+public:
+    GLuint ID;
+
+    EBO(GLuint *indices, GLsizeiprt size)
     {
-      // My dumbass forgot that 4.6 is DSA :P, so gotta remove most of the
-      // binds and what not
-      // Also btw we dont rlly need any other
-      // functions so eh
-      glCreateBuffers(1, &ID);
-      glNamedBufferData(EBO, sizeof(indices), indices, GL_STATIC_DRAW);
+        // My dumbass forgot that 4.6 is DSA :P, so gotta remove most of the
+        // binds and what not
+        // Also btw we dont rlly need any other
+        // functions so eh
+        glCreateBuffers(1, &ID);
+        glNamedBufferData(EBO, sizeof(indices), indices, GL_STATIC_DRAW);
     }
 
-    void LinkEBOtoVAO(GLuint* vaoToLink, GLuint* eboToLink)
+    void LinkEBOtoVAO(GLuint *vaoToLink, GLuint *eboToLink)
     {
-      glVertexArrayElementBuffer(vaoToLink, eboToLink);⎄
+        glVertexArrayElementBuffer(vaoToLink, eboToLink);
     }
 
     // my dumbass was about to write a function to
