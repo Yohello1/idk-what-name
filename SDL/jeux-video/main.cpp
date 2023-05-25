52687dee55b032c25de6f05a4582f72279157ff5
Yohello1 <mp2702737@gmail.com>
2023-05-25 12:14:56 -0400

    Dont get attached to ppl or smth

--- SDL/jeux-video/main.cpp
+++ SDL/jeux-video/main.cpp
@@ -12,19 +12,11 @@
 #include "render/VBO.cpp"
 #include "render/EBO.cpp"
 
-// Simulated array everything is being fed
-cell_t pixels[LOGICAL_WINDOW_WIDTH][LOGICAL_WINDOW_WIDTH];
-// cell new_version[LOGICAL_WINDOW_WIDTH][LOGICAL_WINDOW_WIDTH];
-// Array responsble for the UI
-// This stuff's apparently never defined LOL
-// struct colour user_interface[LOGICAL_WINDOW_WIDTH][LOGICAL_WINDOW_WIDTH];
-// USR input and changes, TODO SWITCH TO EQAUTION
-cell_t usr_input[LOGICAL_WINDOW_WIDTH][LOGICAL_WINDOW_WIDTH];
-bool changed[LOGICAL_WINDOW_WIDTH][LOGICAL_WINDOW_WIDTH];
-cell_t render[LOGICAL_WINDOW_WIDTH][LOGICAL_WINDOW_WIDTH];
-void excecution_finished();
-void draw_box(cord_2d start, cord_2d end);
 
+std::atomic<bool> kys; // politely :3
+unsigned int sartedTime = (unsigned int)time(NULL);
+
+// ECS stuff, dont remove them, the system will kill itself
 std::vector<ui::single_ui_element *> ui_elements;
 unsigned int currenttime = (unsigned int)time(NULL);
 std::mutex mtx2;
@@ -33,6 +25,60 @@ entites::Coordinator Conductor;
 int main()
 {
     std::cout << "Hello World" << std::endl;
+
+    // Creating the temp planes
+    GLfloat vertices[]
+    {
+        // X,     Y,    Z,    U,    V
+        -0.5f,-0.5f, 0.0f, 0.0f, 0.0f,
+        -0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+         0.5f, 0.5f, 0.0f, 1.0f, 1.0f,
+         0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
+    };
+
+    GLuint indices[] =
+    {
+        0,2,1,
+        0,3,2
+    };
+
+    std::cout << " Verticies created?" << '\n';
+    std::cout << "Creating window \n";
+
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(ACTUAL_WINDOW_WIDTH, ACTUAL_WINDOW_HEIGH, "Augustimo", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Window could not be created (GLFW) \n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    gladLoadGL();
+    glViewport(0,0, ACTUAL_WINDOW_WIDTH, ACTUAL_WINDOW_HEIGH);
+    std::cout << "Window has been created\n";
+
+    // put the write the code related to
+    // loading the shaders here
+
+    while(!kys)
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+        if(glfwWindowShouldClose(window))
+        {
+            kys = true;
+        }
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 
 
 }
--- SDL/jeux-video/render/VAO.cpp
+++ SDL/jeux-video/render/VAO.cpp
@@ -1,5 +1,3 @@
-#include "VAO.hpp"
-
 class VAO
 {
 private:
@@ -17,29 +15,29 @@ public:
     // which is associated to the generic
     // vertex attribute
     // is 0, cause ion have the sanity
-    void enableArrayIndex(GLuint *vaoIn, GLuint index)
+    void enableArrayIndex(GLuint& vaoIn, GLuint index)
     {
         glEnableVertexArrayAttrib(vaoIn, index);
-        glVertexArrayAttribBinding(vaoInt, 0, 0);
+        glVertexArrayAttribBinding(vaoIn, 0, 0);
     }
 
     // The offset is the number of floats to make it there
     // NOT THE ACTUAL SIZE
     // As in the offset is multiplied by the size of the a GLfloat
     // So like, offset*sizeof(GLfloat);
-    void setArrayFormat(GLuint *vaoIn, GLuint index, GLuint size, GLuint offset)
+    void setArrayFormat(GLuint& vaoIn, GLuint index, GLuint size, GLuint offset)
     {
-        glVertexArrayAttribFormat(vaoIn, index, size, GL_FLOAT, GL_FALSE, offset(sizeof(GLfloat)));
+        glVertexArrayAttribFormat(vaoIn, index, size, GL_FLOAT, GL_FALSE, offset*(sizeof(GLfloat)));
     }
 
     // You have to compute the stride urself here btw, cause ion have the sanity
-    void linkVBO(GLuint *vaoIn, GLuint index, GLuint *vboIn, GLuint offset, GLsizei stride)
+    void linkVBO(GLuint& vaoIn, GLuint index, GLuint& vboIn, GLuint offset, GLsizei stride)
     {
         glVertexArrayVertexBuffer(vaoIn, index, vboIn, offset, stride);
     }
 
-    void linkEBO(GLuint *vaoIn, GLuint *eboIn)
+    void linkEBO(GLuint& vaoIn, GLuint& eboIn)
     {
-        glVertexArrayElementBuffer(VAO, EBO);
+        glVertexArrayElementBuffer(vaoIn, eboIn);
     }
 };
