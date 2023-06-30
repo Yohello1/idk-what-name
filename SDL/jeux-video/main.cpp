283fd682ae4dce46a5fc944de0a6f13eecb684ba
Yohello1 <mp2702737@gmail.com>
2023-06-30 00:48:12 -0400

    Removed the backslashn's

--- SDL/jeux-video/main.cpp
+++ SDL/jeux-video/main.cpp
@@ -138,20 +138,20 @@ int main()
 	// compute shader
 	GLuint ComputeShader;
 	{
-		const char* ComputeShaderSource = R"(\n
-		\#version 460 core\n
+		const char* ComputeShaderSource = R"(
+		#version 460 core
 
-		layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in\n
-		layout(rgba32f, binding = 0) uniform image2D imgOutput;\n
+		layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in
+		layout(rgba32f, binding = 0) uniform image2D imgOutput;
 
-		void main()\n
+		void main()
 		{\n
-			vec4 value = vec4(0.0,0.0,0.0,1.0);\n
-			ivec2 texelCoord = ivec2(gl_GlobalInvocationID.xy);\n
+			vec4 value = vec4(0.0,0.0,0.0,1.0);
+			ivec2 texelCoord = ivec2(gl_GlobalInvocationID.xy);
 
-			value.x = float(texelCoord.x)/(gl_NumWorkGroups.x);\n
-			value.x = float(texelCoord.y)/(gl_NumWorkGroups.y);\n
-		}\n)";
+			value.x = float(texelCoord.x)/(gl_NumWorkGroups.x);
+			value.x = float(texelCoord.y)/(gl_NumWorkGroups.y);
+		})";
 
 		std::cout << ComputeShaderSource << '\n';
 
