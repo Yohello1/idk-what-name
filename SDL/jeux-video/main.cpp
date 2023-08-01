81728f3f6c512e0c48220fcc316652c1c64be50c
Yohello1 <mp2702737@gmail.com>
2023-07-31 23:50:41 -0400

    when u relise they never liked you

--- SDL/jeux-video/main.cpp
+++ SDL/jeux-video/main.cpp
@@ -44,6 +44,7 @@ GLuint indices[] =
 
 int main()
 {
+	std::cout << "IMG MADE" << '\n';
 	glfwInit();
 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
@@ -94,31 +95,24 @@ int main()
 	std::cout << "vert and frag shaders have been made" << std::endl;
 
 	Shaders::computeShader dejaVu("render/shaders/compute.glsl");
-	Shaders::computeImage halfwayThroughNovember(1024,1024);
+	Shaders::computeImageOut halfwayThroughNovember(1024,1024,0);
+	Shaders::computeImageIn heyworld(1024,1024,1);
 
-	// int work_grp_cnt[3];
-	// glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_COUNT, 0, &work_grp_cnt[0]);
-	// glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_COUNT, 1, &work_grp_cnt[1]);
-	// glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_COUNT, 2, &work_grp_cnt[2]);
-	// std::cout << "Max work groups per compute shader" <<
-	// 	" x:" << work_grp_cnt[0] <<
-	// 	" y:" << work_grp_cnt[1] <<
-	// 	" z:" << work_grp_cnt[2] << "\n";
 
-	// int work_grp_size[3];
-	// glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_SIZE, 0, &work_grp_size[0]);
-	// glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_SIZE, 1, &work_grp_size[1]);
-	// glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_SIZE, 2, &work_grp_size[2]);
-	// std::cout << "Max work group sizes" <<
-	// 	" x:" << work_grp_size[0] <<
-	// 	" y:" << work_grp_size[1] <<
-	// 	" z:" << work_grp_size[2] << "\n";
 	dejaVu.printMaxComputeSize();
-
-
 	int work_grp_inv;
 	glGetIntegerv(GL_MAX_COMPUTE_WORK_GROUP_INVOCATIONS, &work_grp_inv);
 	std::cout << "Max invocations count per work group: " << work_grp_inv << "\n";
+
+
+	// fake image maker :)
+	float* fakeImg = new float[1024*1024*4];
+	functions::fakeRandomImage(fakeImg);
+	std::cout << "IMG MADE2" << '\n';
+
+	heyworld.copyDataFloat(fakeImg);
+
+
 
 
 	while (!glfwWindowShouldClose(window))
