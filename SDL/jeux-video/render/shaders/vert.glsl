cec2df990ac5b77012feb9cc2553762729e1d3ac
Yohello1 <mp2702737@gmail.com>
2023-08-05 15:06:51 -0400

    rotate banana

--- SDL/jeux-video/render/shaders/vert.glsl
+++ SDL/jeux-video/render/shaders/vert.glsl
@@ -2,13 +2,14 @@
 layout (location = 0) in vec3 pos; // vert 3d position
 layout (location = 1) in vec2 uvs; // vert texture position
 
-// uniform mat4 uProjectionMatrix;
+uniform mat4 uProjectionMatrix;
 uniform mat4 uViewMatrix;
+uniform mat4 uModelMatrix;
 
 out vec2 UVs;
 void main()
 {
-	gl_Position = uViewMatrix * vec4(pos, 1.0); // btw it has to be projection*view*vec4(1)
-	// gl_Position = vec4(pos.x, pos.y, pos.z, 1.000);
+	// gl_Position = uViewMatrix * vec4(pos, 1.0); // btw it has to be projection*view*vec4(1)
+	gl_Position = uProjectionMatrix * uViewMatrix * uModelMatrix * vec4(pos.x, pos.y, pos.z, 1.000);
 	UVs = uvs;
 }
