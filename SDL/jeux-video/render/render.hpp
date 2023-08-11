4ff2c935cb021ccdf82cace11bdbf39266c97d01
Yohello1 <mp2702737@gmail.com>
2023-08-11 01:31:15 -0400

    YOU AND I-I-I-I

--- SDL/jeux-video/render/render.hpp
+++ SDL/jeux-video/render/render.hpp
@@ -1,8 +1,19 @@
-#ifndef RENDER_H_
-#define RENDER_H_
-// I pray no one ever gets an error from this file
-// #include "VAO.hpp"
-// #include "VBO.hpp"
-// #include "EBO.hpp"
+// I should at some point go through this and fix all the naming convention stuff
+// but i cannot be bothered to rn
+namespace render
+{
+    // ok now lets do the
+    // thinking
+    // To render we have to copy the data
+    // from several 'textures'/maps to the gpu
+    // and then it just does the math required
+    // to turn that into the final product
+    // So what I need is a function which
+    // just fetches the data from the cell data
+    // type, and from the rgb data type
+    // Also needs to just edit a specific rang ofc
+    //
+    // We'll also need to leave the alpha channel open
+    // to being changed, cause like compositing sanity
 
-#endif // RENDER_H_
+}
