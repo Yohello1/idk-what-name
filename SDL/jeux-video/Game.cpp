68277370ed26ff3c44a3627858925d121426a03c
Yohello1 <mp2702737@gmail.com>
2023-08-31 01:43:06 -0400

    weak knees, hands, heart

---
+++ SDL/jeux-video/Game.cpp
@@ -0,0 +1,28 @@
+// I lost something :P
+
+namespace Game
+{
+    class Game
+    {
+        // lmao i kinda wanna talk abt jace here
+        // But I know he'll probs read this and get mad
+        // over what I write, even tho it's from my
+        // perspective
+        // anyways, it kinda feels like he's
+        // using me, which i kinda expected
+        // my stomach was right :P
+        // if future me comes back, it's cause
+        // he only comes when he needs help with something
+        // and doesnt care abt anything else
+
+
+        public:
+            Game(const char* gameName, unsigned int map_x, unsigned int map_y) // wtf else do I put LMAO
+            {
+                computeImages = new float[map_x*map_y*4];
+            }
+            
+        private:
+            float* computeImages;
+    };
+}
