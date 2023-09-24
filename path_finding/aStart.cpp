8dd4cbaca5ff42636571f6beeeacb4f6d769e112
Yohello1 <mp2702737@gmail.com>
2023-09-23 21:23:25 -0400

    Archive this before I delete all of this

--- path_finding/aStart.cpp
+++ path_finding/aStart.cpp
@@ -25,10 +25,10 @@ struct cord_2d
 
 struct aStarFinding
 {
-    bool inert;
     cord_2d pixel_parent;
     uint16_t HCost;
     uint16_t GCost;
+    uint16_t fCost;
 };
 
 struct pixel_data_struct
@@ -50,7 +50,13 @@ struct boids
     uint8_t b;
     uint8_t a;
 
-    void pathFind(cord_2d endPoint);
+    int path_find(cord_2d endPoint)
+    {
+        // https://medium.com/@nicholas.w.swift/easy-a-star-pathfinding-7e6689c7f7b2
+        std::vector<unique_ptr>
+
+
+    }
 };
 
 struct pixel_data_struct pixels[LOGICAL_WINDOW_WIDTH][LOGICAL_WINDOW_WIDTH];
@@ -61,16 +67,13 @@ struct aStarFinding shadeOfBlue[LOGICAL_WINDOW_WIDTH][LOGICAL_WINDOW_WIDTH];
 void scr_dump();
 void redraw_and_render();
 void excecution_finished();
+void draw_birds();
+void reset_astar_array();
 int noise_gen();
 // this script is gonn hurtme
 // I havent listened to kms - sub urban in a while
-
-
-
-
 // ion feel like taking usr input so these are just gonna be pre-defined global variables
 const int noise_density = 90;
-double wind_dir_degrees = 55;
 
 int main()
 {
@@ -115,11 +118,30 @@ int main()
             }
         }
     }
+
+    std::cout << "why is it not sending" << std::endl;
+
+    for(int i = 0; i < (sizeof(birds))/(sizeof(uint8_t)*8); i++)
+    {
+        birds[i].r = (rand() % 255);
+        birds[i].g = (rand() % 255);
+        birds[i].b = (rand() % 255);
+        birds[i].position.x_pos = (rand() % LOGICAL_WINDOW_WIDTH);
+        birds[i].position.y_pos = (rand() % LOGICAL_WINDOW_WIDTH);
+
+    }
+
+    std::cout << "visual" << std::endl;
     // redraw_and_render();
     // uh does this make sense?
     bool kys = false;
     while(!kys)
     {
+
+        // CODE HERE
+        // Ok first of all, let's make the birds
+        //
+
         redraw_and_render();
 
         SDL_Delay(10);
@@ -155,9 +177,14 @@ void redraw_and_render()
         for (int y_pos = 0; y_pos < LOGICAL_WINDOW_WIDTH; y_pos++)
         {
             SDL_SetRenderDrawColor(renderer, pixels[x_pos][y_pos].r, pixels[x_pos][y_pos].g, pixels[x_pos][y_pos].b, pixels[x_pos][y_pos].a);
+            if(pixels[x_pos][y_pos].inert == true)
+            {
+                SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+            }
             SDL_RenderDrawPoint(renderer, x_pos, y_pos);
         }
     }
+    draw_birds();
     SDL_RenderPresent(renderer);
 }
 void excecution_finished(void)
@@ -175,6 +202,29 @@ void excecution_finished(void)
     SDL_DestroyWindow(window);
     SDL_Quit();
 } //                                                                                                                                                                                                                                                                                                                                  v
+void draw_birds()
+{
+    for(uint i = 0; i < (sizeof(birds))/(sizeof(uint8_t)*8); i++)
+    {
+        SDL_SetRenderDrawColor(renderer, birds[i].r, birds[i].g, birds[i].b, birds[i].a);
+        SDL_RenderDrawPoint(renderer, birds[i].position.x_pos, birds[i].position.y_pos);
+    }
+}
+void reset_astar_array()
+{
+    memset(&shadeOfBlue, 0, sizeof(shadeOfBlue));
+}
+void pathFindChildren(cord_2d endPoint)
+{
+    for(uint i = 0; i < (sizeof(birds))/(sizeof(uint8_t)*8); i++)
+    {
+        reset_astar_array();
+        shadeOfBlue[birds[i].position.x_pos][birds[i].position.y_pos].GCost = 0;
+        shadeOfBlue[birds[i].position.x_pos][birds[i].position.y_pos].HCost = 0;
+        birds[i].path_find(endPoint);
+    }
+
+}
 
 int noise_gen()
 {
