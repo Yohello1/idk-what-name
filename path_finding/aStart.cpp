54a252d2d2ded09bca1dc7a116c18456bad5df46
Yohello1 <mp2702737@gmail.com>
2023-10-02 23:45:31 -0400

    Now displaying map, start, and end points

--- path_finding/aStart.cpp
+++ path_finding/aStart.cpp
@@ -4,41 +4,66 @@
 #include <queue>
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <cstring>
+#include <random>
 
-// #include "randomStuff/DrawMap.cpp"
+sf::RenderWindow window(sf::VideoMode(1024,1024), "SFML works!");
+sf::Texture texture;
+sf::Sprite unvisted, visited, start, end, path, wall;
+
+std::array<std::array<int, 128>, 128> map;
+
+
+void drawMap();
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1024,1024), "SFML works!");
 
     // Load texture
-    sf::Texture texture;
-    if(!texture.loadFromFile("randomStuff/tileSprite.png")) { std::cout << "file did not infact load" << std::endl; }
-    texture.setSmooth(true);
-
+    {
+        if(!texture.loadFromFile("randomStuff/tileSprite.png")) { std::cout << "file did not infact load" << std::endl; }
+        texture.setSmooth(true);
+    }
     // Setting sprites
-    sf::Sprite unvisted, visited, start, end, path;
     {
         unvisted.setTexture(texture);
-        unvisted.setTextureRect(sf::IntRect(0, 0, 32, 32));
+        unvisted.setTextureRect(sf::IntRect(0, 0, 8, 8));
         // window.draw(unvisted);
 
         visited.setTexture(texture);
-        visited.setTextureRect(sf::IntRect(32,0,32,32));
+        visited.setTextureRect(sf::IntRect(8,0,8,8));
 
         start.setTexture(texture);
-        start.setTextureRect(sf::IntRect(64,0,32,32));
+        start.setTextureRect(sf::IntRect(16,0,8,8));
 
         end.setTexture(texture);
-        end.setTextureRect(sf::IntRect(96,0,32,32));
+        end.setTextureRect(sf::IntRect(24,0,8,8));
 
         path.setTexture(texture);
-        path.setTextureRect(sf::IntRect(128,0,32,32));
+        path.setTextureRect(sf::IntRect(32,0,8,8));
+
+        wall.setTexture(texture);
+        wall.setTextureRect(sf::IntRect(40,0,8,8));
     }
-    // sf::CircleShape shape(100.f);
-    // shape.setFillColor(sf::Color::Green);
+    // Filling array
+    {
+        // map.fill(0);
+        memset( &map[0][0], 0, sizeof(map) );
+    }
+    // placing start & end points
+    {
+        srand(time(0));
+
+        map[rand() % 128][rand() % 128] = 2;
+        map[rand() % 128][rand() % 128] = 3;
+    }
+    
 
     while (window.isOpen())
     {
+        window.clear();
+
+
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -46,10 +71,71 @@ int main()
                 window.close();
         }
 
-        window.clear();
-        window.draw(path);
+        drawMap();
+
         window.display();
     }
 
     return 0;
+}
+
+void drawMap()
+{
+    for(int x_pos = 0; x_pos < 128; x_pos++)
+    {
+        for(int y_pos = 0; y_pos < 128; y_pos++)
+        {
+
+            // let's say
+            // 0 = unvisted
+            // 1 = visted
+            // 2 = start
+            // 3 = end
+            // 4 = path
+            // 5 = wall
+
+            // Unvisted
+            if(map[x_pos][y_pos] == 0)
+            {
+                unvisted.setPosition(x_pos*8,y_pos*8);
+                window.draw(unvisted);
+            }
+
+            // Visted
+            else if(map[x_pos][y_pos] == 1)
+            {
+                visited.setPosition(x_pos*8,y_pos*8);
+                window.draw(visited);
+            }
+
+            // Start
+            else if(map[x_pos][y_pos] == 2)
+            {
+                start.setPosition(x_pos*8,y_pos*8);
+                window.draw(start);
+            }
+
+            // end
+            else if(map[x_pos][y_pos] == 3)
+            {
+                end.setPosition(x_pos*8,y_pos*8);
+                window.draw(end);
+            }
+
+            // Path
+            else if(map[x_pos][y_pos] == 4)
+            {
+                path.setPosition(x_pos*8,y_pos*8);
+                window.draw(path);
+            }
+
+
+            else if(map[x_pos][y_pos] == 5)
+            {
+                wall.setPosition(x_pos*8,y_pos*8);
+                window.draw(wall);
+            }
+
+        }
+    }
 }
