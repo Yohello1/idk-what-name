417e6626e9c997b5e2be676b5e91c0bf9388647b
Yohello1 <mp2702737@gmail.com>
2023-10-02 23:08:49 -0400

    Sprites added

--- path_finding/aStart.cpp
+++ path_finding/aStart.cpp
@@ -3,13 +3,39 @@
 #include <chrono>
 #include <queue>
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
-#include "randomStuff/DrawMap.cpp"
+// #include "randomStuff/DrawMap.cpp"
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(1024,1024), "SFML works!");
+
+    // Load texture
+    sf::Texture texture;
+    if(!texture.loadFromFile("randomStuff/tileSprite.png")) { std::cout << "file did not infact load" << std::endl; }
+    texture.setSmooth(true);
+
+    // Setting sprites
+    sf::Sprite unvisted, visited, start, end, path;
+    {
+        unvisted.setTexture(texture);
+        unvisted.setTextureRect(sf::IntRect(0, 0, 32, 32));
+        // window.draw(unvisted);
+
+        visited.setTexture(texture);
+        visited.setTextureRect(sf::IntRect(32,0,32,32));
+
+        start.setTexture(texture);
+        start.setTextureRect(sf::IntRect(64,0,32,32));
+
+        end.setTexture(texture);
+        end.setTextureRect(sf::IntRect(96,0,32,32));
+
+        path.setTexture(texture);
+        path.setTextureRect(sf::IntRect(128,0,32,32));
+    }
+    // sf::CircleShape shape(100.f);
+    // shape.setFillColor(sf::Color::Green);
 
     while (window.isOpen())
     {
@@ -21,7 +47,7 @@ int main()
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(path);
         window.display();
     }
 
