3e8f45469e9d81e606444b11675a29fe5b554cc7
Yohello1 <mp2702737@gmail.com>
2023-10-30 00:00:57 -0400

    No idea what was added

--- path_finding/leaderMazeAStar.cpp
+++ path_finding/leaderMazeAStar.cpp
@@ -11,7 +11,7 @@
 #include <cmath>
 
 #define MAP_SIZE 128
-#define LEADER_AMT 1000 // crashes above 25, no idea why
+#define LEADER_AMT 10000 // crashes above 25, no idea why
 
 struct Comparator {
     bool operator()(std::tuple<double, double, std::pair<int, int>>& t1, std::tuple<double, double, std::pair<int, int>>& t2) {
@@ -86,8 +86,6 @@ int main()
         window.clear();
         drawMap();
         window.display();
-
-        // sleep(3);
 
         if(closeOpenSpaces({rand() % MAP_SIZE, rand() % MAP_SIZE}) < 8192)
         {
