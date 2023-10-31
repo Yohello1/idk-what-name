3aba6e0143775bb7d9bee32ed1ea9def41f52eae
Yohello1 <mp2702737@gmail.com>
2023-10-31 12:00:29 -0400

    Uploading data

--- path_finding/pathFinding.sh
+++ path_finding/pathFinding.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo 'hai'
-for i in {10..100}
+for i in {5..5000..2}
 do
 	g++ -Wall -DLEADER_AMT=$i -g -c leaderPathFinding.cpp
-	g++ leaderPathFinding.cpp -Wall -g -lsfml-graphics -lsfml-window -lsfml-system -o draw2
+	g++ leaderPathFinding.cpp -DLEADER_AMT=$i -Wall -g -lsfml-graphics -lsfml-window -lsfml-system -o draw2
 	printf $i >> data/leaderPath.txt;
 	printf %s ", " >> data/leaderPath.txt;
 	./draw2 >> data/leaderPath.txt;
