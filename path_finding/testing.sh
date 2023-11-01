cb1fe1f86835d5911d6ca29117a7d7f9cf99f144
Yohello1 <mp2702737@gmail.com>
2023-11-01 09:58:20 -0400

    Final commit

--- path_finding/testing.sh
+++ path_finding/testing.sh
@@ -3,10 +3,10 @@ echo 'hai'
 for i in {10..10000..1}
 do
 	g++ -Wall -DLEADER_AMT=$i -g -c leaderMazeAStar.cpp
-	g++ leaderMazeAStar.o -Wall -g -lsfml-graphics -lsfml-window -lsfml-system -o draw2
+	g++ leaderMazeAStar.o -Wall -g -lsfml-graphics -lsfml-window -lsfml-system -o draw3
 	printf $i >> data/aStarData.txt;
 	printf %s ", " >> data/aStarData.txt;
-	./draw2 >> data/aStarData.txt;
+	./draw3 >> data/aStarData.txt;
 	echo $i;
 done
 
