ef8504fa14af2da94f5625fb6e7b8314cab796da
Yohello1 <mp2702737@gmail.com>
2023-11-05 00:28:00 -0400

    Second last data push

--- path_finding/pathFinding.sh
+++ path_finding/pathFinding.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo 'hai'
-for i in {5..5000..2}
+for i in {1..1000..2}
 do
-	g++ -Wall -DLEADER_AMT=$i -g -c leaderPathFinding.cpp
-	g++ leaderPathFinding.cpp -DLEADER_AMT=$i -Wall -g -lsfml-graphics -lsfml-window -lsfml-system -o draw2
+	g++ -Wall -DLEADER_AMT=$i -DBIRD_AMT=10 -g -c leaderPathFinding.cpp
+	g++ leaderPathFinding.cpp -DLEADER_AMT=$i -DBIRD_AMT=10 -Wall -g -lsfml-graphics -lsfml-window -lsfml-system -o draw2
 	printf $i >> data/leaderPath.txt;
 	printf %s ", " >> data/leaderPath.txt;
 	./draw2 >> data/leaderPath.txt;
