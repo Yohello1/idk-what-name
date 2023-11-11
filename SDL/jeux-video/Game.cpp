c9978f0fa01971b9e0ca30afabb49aa7103d48ea
Yohello1 <mp2702737@gmail.com>
2023-11-11 00:48:57 -0500

    Do they really want to talk to me?

--- SDL/jeux-video/Game.cpp
+++ SDL/jeux-video/Game.cpp
@@ -19,22 +19,64 @@ namespace Game
         */
 
         public:
-            Game(const char* gameName, const char* sceneName, unsigned int map_x, unsigned int map_y) // wtf else do I put LMAO
-            {
-                std::memcpy(&name, sceneName, sizeof(sceneName));
+        // Yes these are public for simplicites sake
+        float* layers;
+        MVPMatrix::MVPMatrixes* transformMatricies;
 
-                background = new float[map_x*map_y*4];
-                foreground = new float[map_x*map_y*4];
-                effects = new float[map_x*map_y*4];
-                userInterface = new float[map_x*map_y*4];
-            }
-            
+        Game(std::string sceneName, unsigned int map_x, unsigned int map_y, uint8_t layersAmt, int aspect, int width, int height, int farCloseDistance) // wtf else do I put LMAO
+        {
+            name = sceneName;
+            mapX = map_x;
+            mapY = map_y;
+
+            layers = new float[map_x*map_y*4*layersAmt];
+
+            tansformMatricies = MVPMatrix::MVPMatrixes(aspect, width, height, farCloseDistance);
+        }
+
+        // layerNum is the layer number
+        // Offset is the offset of the actual data, assuming in the same layer
+        // There are no validity checks :P
+        // value is the value it will be updated to
+        void updateValue(int layerNum, int offset, float value)
+        {
+            layers[offset+layerNum*mapX*mapY*4] = value;
+        }
+
+        // No validity check, part is the like individual offset(?)
+        // like 0,1,2,3
+        unsigned int findOffset(unsigned int row, unsigned int col, unsigned int part)
+        {
+            unsigned int temp = row*mapX*4 + 4*col + part;
+            return temp;
+        }
+
+        bool updateImageBulk(int layerNum, float* data)
+        {
+            // here memcpy
+            std::copy(data, data + mapX*mapY*4, layers + mapX*mapY*4*layerNum);
+
+        }
+
+        // TODO:
+        // write image update functions,                    - Done
+        // functions to change individual elements/parts,   - Done
+        // function to fetch/update matrices,
+        // function to get scene name                       - Done
+
+
+        // I cannot be bothered to get this working
+        std::string getName()
+        {
+            // if(name == NULL)
+            //     return "";
+            return name;
+        }
+
         private:
-            const char* name[64];
+        std::string name;
+        unsigned int mapX;
+        unsigned int mapY;
 
-            uint16_t* background;
-            uint16_t* foreground;
-            uint16_t* effects;
-            uint16_t* userInterface;
     };
 }
