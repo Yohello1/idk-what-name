c0bbe53650f880cc19331cc32caaf91c59620fcd
Yohello1 <mp2702737@gmail.com>
2023-12-01 22:18:17 -0500

    why wont they even reply anymore

--- SDL/jeux-video/Game.cpp
+++ SDL/jeux-video/Game.cpp
@@ -20,56 +20,47 @@ namespace Game
 
         public:
         // Yes these are public for simplicites sake
-        std::vector<std::unique_ptr<float*>> layers;
-        MVPMatrix::MVPMatrixes* transforms;
+        std::vector<float*> _layers; // i have no idea how to use unique pointers correctly here :P
+        MVPMatrix::MVPMatrixes* _transforms;
 
         Game(std::string sceneName, unsigned int map_x, unsigned int map_y, uint8_t layersAmt, int aspect, int width, int height, int farCloseDistance) // wtf else do I put LMAO
         {
-            name = sceneName;
-            mapX = map_x;
-            mapY = map_y;
+            _name = sceneName;
+            _mapX = map_x;
+            _mapY = map_y;
 
             for(uint8_t i = 0; i < layersAmt; i++)
             {
-                layers.push_back(new float[map_x*map_y*4*layersAmt]);
+
+                _layers.push_back(new float[_mapY*_mapY*4*layersAmt]);
             }
 
-            transforms = new MVPMatrix::MVPMatrixes(aspect, width, height, farCloseDistance);
+            _transforms = new MVPMatrix::MVPMatrixes(aspect, width, height, farCloseDistance);
         }
 
         // layerNum is the layer number
         // Offset is the offset of the actual data, assuming in the same layer
         // There are no validity checks :P
         // value is the value it will be updated to
-        void updateValue(int layerNum, int offset, float value)
+        void updateValue(int layerNum, int index, float value)
         {
-            // layers[offset+layerNum*mapX*mapY*4] = value;
-
-        }
-
-        // No validity check, part is the like individual offset(?)
-        // like 0,1,2,3
-        unsigned int findOffset(unsigned int row, unsigned int col, unsigned int part)
-        {
-            unsigned int temp = row*mapX*4 + 4*col + part;
-            return temp;
+            _layers.at(layerNum)[index] = value;
         }
 
         void updateImageBulk(int layerNum, float* data)
         {
             // here memcpy
-            std::copy(data, data + mapX*mapY*4, layers.at(layerNum));
-
+            std::copy(data, data + _mapX*_mapY*4, _layers.at(layerNum));
         }
 
         void rotateView(float x, float y, float z)
         {
-            transforms->rotateView(x,y,z);
+            _transforms->rotateView(x,y,z);
         }
 
         void translateView(float x, float y, float z)
         {
-            transforms->translateView(x,y,z);
+            _transforms->translateView(x,y,z);
         }
 
 
@@ -78,27 +69,27 @@ namespace Game
         {
             // if(name == NULL)
             //     return "";
-            return name;
+            return _name;
         }
 
         void changeTitle(GLFWwindow* window)
         {
-            glfwSetWindowTitle(window, name.c_str());
+            glfwSetWindowTitle(window, _name.c_str());
         }
 
         ~ Game()
         {
             // tiddy muncher 5000
-            for(int i = layers.size(); i > 0; i--)
+            for(int i = _layers.size(); i > 0; i--)
             {
-                free(layers.at(i));
+                free(_layers.at(0));
             }
         }
 
         private:
-        std::string name;
-        unsigned int mapX;
-        unsigned int mapY;
+        std::string _name;
+        unsigned int _mapX;
+        unsigned int _mapY;
 
     };
 }
--- SDL/jeux-video/render/render.hpp
+++ SDL/jeux-video/render/render.hpp
@@ -1,8 +1,16 @@
-#ifndef RENDER_H_
-#define RENDER_H_
-// I pray no one ever gets an error from this file
 // #include "VAO.hpp"
 // #include "VBO.hpp"
 // #include "EBO.hpp"
 
-#endif // RENDER_H_
+namespace render
+{
+    class engineContainer
+    {
+        GLuint VAO, VBO, EBO;
+
+        engineContainer()
+        {
+
+        }
+    };
+}
