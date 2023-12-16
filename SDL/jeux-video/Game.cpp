2e4ea34037db198a49b377c2d6a12ff19ce1ecc8
Yohello1 <mp2702737@gmail.com>
2023-12-15 21:01:52 -0500

    I should like not exist, not with the things I do

--- SDL/jeux-video/Game.cpp
+++ SDL/jeux-video/Game.cpp
@@ -22,6 +22,7 @@ namespace Game
         // Yes these are public for simplicites sake
         std::vector<float*> _layers; // i have no idea how to use unique pointers correctly here :P
         MVPMatrix::MVPMatrixes* _transforms;
+        GLuint VAO, VBO, EBO;
 
         Game(std::string sceneName, unsigned int map_x, unsigned int map_y, uint8_t layersAmt, int aspect, int width, int height, int farCloseDistance) // wtf else do I put LMAO
         {
@@ -31,11 +32,55 @@ namespace Game
 
             for(uint8_t i = 0; i < layersAmt; i++)
             {
-
                 _layers.push_back(new float[_mapY*_mapY*4*layersAmt]);
             }
 
             _transforms = new MVPMatrix::MVPMatrixes(aspect, width, height, farCloseDistance);
+
+
+
+            // we do not talk abt this
+            //
+            GLfloat vertices[] =
+                {
+                    -64.0f, -64.0f , 0.0f, 0.3f, 0.0f,
+                    -64.0f,  64.0f , 0.0f, 0.3f, 1.0f,
+                    64.0f,  64.0f , 0.0f, 1.0f, 1.0f,
+                    64.0f, -64.0f , 0.0f, 1.0f, 0.0f,
+
+                    -64.0f, -64.0f , 5.0f, 0.0f, 0.0f,
+                    -64.0f,  64.0f , 5.0f, 0.0f, 1.0f,
+                    64.0f,  64.0f , 5.0f, 1.0f, 1.0f,
+                    64.0f, -64.0f , 5.0f, 1.0f, 0.0f,
+                };
+
+            GLuint indices[] =
+                {
+                    0, 2, 1,
+                    0, 3, 2,
+                    4, 6, 5,
+                    4, 7, 6
+                };
+            // VBO and EBO stuff
+            {
+                glCreateVertexArrays(1, &VAO);
+                glCreateBuffers(1, &VBO);
+                glCreateBuffers(1, &EBO);
+
+                glNamedBufferData(VBO, sizeof(vertices), vertices, GL_STATIC_DRAW);
+                glNamedBufferData(EBO, sizeof(indices), indices, GL_STATIC_DRAW);
+
+                glEnableVertexArrayAttrib(VAO, 0);
+                glVertexArrayAttribBinding(VAO, 0, 0);
+                glVertexArrayAttribFormat(VAO, 0, 3, GL_FLOAT, GL_FALSE, 0);
+
+                glEnableVertexArrayAttrib(VAO, 1);
+                glVertexArrayAttribBinding(VAO, 1, 0);
+                glVertexArrayAttribFormat(VAO, 1, 2, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat));
+
+                glVertexArrayVertexBuffer(VAO, 0, VBO, 0, 5 * sizeof(GLfloat));
+                glVertexArrayElementBuffer(VAO, EBO);
+            }
         }
 
         // layerNum is the layer number
