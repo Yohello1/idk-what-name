a58164d1d446bfe78d81946d5b0e2b39b16066ff
Yohwllo <mp2702737@gmail.com>
2024-03-09 19:01:22 -0500

    i hope she didnt commit suicide, and and just said she'd be gone so I wouldnt be sad

--- testing/jesse.cpp
+++ testing/jesse.cpp
@@ -4,125 +4,69 @@
 #include <cmath>
 #include <algorithm>
 #include <numeric>
+#include <string>
+#include <vector>
 
-uint64_t calculateDeterminite(uint64_t i, uint64_t j, uint64_t k, uint64_t l)
+uint64_t seed, multiplier, moduloVal, addedVal;
+uint32_t amtVals = 4294967295;
+std::vector<uint64_t> values;
+
+bool isPrime(int num);
+
+int main(int argc, char *argv[])
 {
-    uint64_t returnVal = std::abs((long long) ((i*k-i*l)+(j*k-j*l)+(j*l-k*k)));
-
-    // returnVal = std::abs(returnVal);
-    return returnVal;
-}
-
-uint64_t factor(uint64_t value)
-{
-    // basically just go till primes LMAO
-    unsigned int sprimes[16] = {0x02,0x03,0x05,0x07,0x0B,0x0D,0x11,0x13,
-                                0x17,0x1D,0x1F,0x25,0x29,0x2B,0x2F,0x35};
-
-    int counter = 0;
-    while(counter < 16)
+    std::cout << "It is suggested that the moduloVal is a prime number" << std::endl;
+    // x_{k+1}  = g*x_{k} % m
+    if(argc == 1)
     {
-        if(value % sprimes[counter] == 0)
-        {
-            value /= sprimes[counter];
-        }
-        else
-        {
-            counter++;
-        }
+        std::cout << "You need to give us three space seperated integers as arguments, [seed] [multiplier] [modulo value]\n";
+        return 0;
     }
 
-    return value;
-}
-
-int GCD(uint64_t a, uint64_t b)
-{
-   if (b == 0)
-   return a;
-   return GCD(b, a % b);
-}
-
-uint64_t calcModulus(uint64_t* list, int length)
-{
-    // GCD stands for greatest common demoninator, which results in whole number
-    uint64_t* tempGCD = new uint64_t[length];
-    for(int i = 0; i < length; i++)
+    // Input
     {
-        tempGCD[i] = list[i];
-    }
+        std::string strSeed(argv[1]);
+        std::string strMultiplier(argv[2]);
+        std::string strModuloValue(argv[3]);
+        std::string strAddedVal(argv[4]);
 
-    for(int k = 1; k < length; k++)
-    {
-        for(int i = 0; i < length-k; i++)
+        seed = std::stoul(strSeed, nullptr);
+        multiplier = std::stoul(strMultiplier, nullptr);
+        moduloVal = std::stoul(strModuloValue, nullptr);
+        addedVal = std::stoul(strAddedVal, nullptr);
+
+        std::cout << seed << ' ' << multiplier << ' ' << moduloVal << std::endl;
+
+        if(!isPrime(moduloVal))
         {
-            tempGCD[i] = std::gcd(tempGCD[i], tempGCD[i+1]);
-            // I did not know the gcd function was built in LMAO
+            std::cout << "Modulo val is NOT a prime" << std::endl;
         }
     }
 
-    return factor(tempGCD[0]);
+
+    // ok gonna compute 2^12 numbers (4096)
+    values.reserve(amtVals);
+    values[0] = seed;
+    for(uint32_t i = 1; i < amtVals;i++)
+    {
+        values[i] = (values[i-1]*multiplier + addedVal) % moduloVal;
+        // std::cout << values[i] << std::endl;
+    }
 }
 
-uint64_t getModulus(uint64_t * detlist, int length)
+
+
+bool isPrime(int num)
 {
-    uint64_t* tempGCD = new uint64_t[length];
-    for(int i = 0;i<length;i++)
+    bool flag = true;
+
+    for(int i = 2; i <= std::sqrt(num); i++)
     {
-        tempGCD[i] = detlist[i];
-    }
-    for(int k = 1;k<length;k++)
-    {
-        for(int i = 0;i<length-k;i++)
+        if(num % i == 0)
         {
-            tempGCD[i] = std::gcd(tempGCD[i],tempGCD[i+1]);
-            std::cout << tempGCD[i] << ' ';
+            flag = false;
+            break;
         }
     }
-    return factor(tempGCD[0]);
-}
-
-
-
-int main()
-{
-    // pre requisite values
-    int outputCount = 8;
-    int determiniteListLength = outputCount - 3;
-    uint64_t* lcgOutput = new uint64_t[outputCount];
-    uint64_t* determiniteList = new uint64_t[determiniteListLength];
-    uint64_t modulus;
-    uint64_t a, k , seed;
-    std::cout << "Creating LCG values" << std::endl;
-
-    // LCG basic
-    uint64_t result = 12345;
-    for (int i = 0; i < outputCount; i++)
-    {
-        // idfk what these values are called, otherwise i'd #define them LMAO
-        result = 16806*result+78124;
-        result %= 46585162;
-        lcgOutput[i] = result;
-        std::cout << result << std::endl;
-    }
-
-    // ok now the solver :))
-    std::cout << "starting the solver" << std::endl;
-
-    for(int i = 0; i < determiniteListLength; i++)
-    {
-        determiniteList[i] = calculateDeterminite(lcgOutput[i], lcgOutput[i+1], lcgOutput[i+2], lcgOutput[i+3]);
-    }
-
-    // output determinteList
-    std::cout << '\n' << "determiniteList" << std::endl;
-    for(int i = 0; i < determiniteListLength; i++)
-    {
-        std::cout << determiniteList[i] << std::endl;
-    }
-
-    // Modulus calc
-    std::cout << '\n' << "Getting Modulus" << std::endl;
-    modulus = getModulus(determiniteList, determiniteListLength);
-    std::cout << modulus << std::endl;
-
+    return flag;
 }
