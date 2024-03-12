b4c679c3becac40c9188d29d13547dfb86751f21
Yohwllo <mp2702737@gmail.com>
2024-03-12 15:01:50 -0400

    I need to clean up,, also someone invited me to their bday

---
+++ testing/monte.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <cstdint>
+
+/**
+ * A linear congruential generator, and its associated solver
+ * @author Yohwllo
+ * @version 1.0
+ * @since 2024-03-01
+ */
+
+std::vector<uint64_t> generatedNumebrs;
+
+
+
+int main()
+{
+    std::cout << "let" << std::endl;
+}
