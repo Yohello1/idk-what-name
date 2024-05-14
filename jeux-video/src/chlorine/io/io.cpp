897318e3b9d492babd997256292fd19a3b3ca3dd
Yohwllo <mp2702737@gmail.com>
2024-05-13 20:41:31 -0400

    I dont know anymore

--- jeux-video/src/chlorine/io/io.cpp
+++ jeux-video/src/chlorine/io/io.cpp
@@ -14,15 +14,29 @@ namespace chlorine::io
         }
 
         // I dont know why but adding `::` to the start makes it works
-        //
         ::std::string line;
+
+        std::vector<std::pair<std::string, std::string>> tempData;
+
         while(std::getline(file, line))
         {
             std::istringstream iss(line);
-            std::string dataName, dataValue;
-            if(!(iss >> dataName >> dataValue)) { break; }
-            logOut->log("Data looks like: " + dataName + " " + dataValue + "\n");
+            std::pair<std::string, std::string> tempPair;
+
+            if(!(iss >> tempPair.first >> tempPair.second)) { break; }
+            logOut->log("Data looks like: " + tempPair.first + " " + tempPair.second + "\n");
+
+            tempData.push_back(tempPair);
         }
+
+        // note: spee ddoesnt matter here :P
+        for(int i = 0; i < tempData.size(); i++)
+        {
+            if(tempData[i].first == "name:")
+                sceneIn.sceneName = tempData[i].second;
+        }
+
+        logOut->log(""+sceneIn.sceneName);
 
         // close file and stuff
 
