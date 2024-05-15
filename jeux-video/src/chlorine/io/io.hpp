69a85d3e8ec8bf033eae2eb7265363fbd4485bf7
Yohwllo <mp2702737@gmail.com>
2024-05-14 23:37:19 -0400

    Never again will I do that

--- jeux-video/src/chlorine/io/io.hpp
+++ jeux-video/src/chlorine/io/io.hpp
@@ -7,8 +7,11 @@
 
 #include <chlorine/logs/logs.hpp>
 #include <chlorine/scene/scene.hpp>
+#include <chlorine/scene/components.hpp>
 
 namespace chlorine::io
 {
-        bool sceneImport(::chlorine::scene::scene sceneIn, std::string filePath, ::chlorine::logging::logBase* logOut);
+    bool componentImport(::chlorine::scene::scene sceneIn, std::string filePath, ::chlorine::logging::logBase* logOut);
+    bool sceneImport(::chlorine::scene::scene sceneIn, std::string filePath, ::chlorine::logging::logBase* logOut);
+    ::chlorine::scene::component* objectFactory(std::string classname, std::string filePath);
 }
--- jeux-video/src/main.cpp
+++ jeux-video/src/main.cpp
@@ -3,7 +3,18 @@
 
 #include <chlorine/logs/logs.hpp>
 #include <chlorine/scene/scene.hpp>
+#include <chlorine/scene/component.hpp>
 #include <chlorine/io/io.hpp>
+
+class boxes : public ::chlorine::scene::component
+{
+public:
+    std::pair<uint32_t, uint32_t> position;
+    void dumpData(::chlorine::logging::logBase* logOut) override
+    {
+        logOut->log("Pos:(" + std::to_string(position.first) + "," + std::to_string(position.second) + ")");
+    }
+};
 
 int main()
 {
@@ -19,5 +30,5 @@ int main()
 
     chlorine::scene::scene tempScene(logOut);
 
-    chlorine::io::sceneImport(tempScene, "../test/world.pcsf", logOut);
+    chlorine::io::sceneImport(tempScene, "../test/Arrowhead.pcsf", logOut);
 }
