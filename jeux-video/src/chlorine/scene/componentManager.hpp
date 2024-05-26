b4d0c2ed3e83dc19388080019fad337268bf32e3
Yohwllo <mp2702737@gmail.com>
2024-05-26 15:55:06 -0400

    hiding

--- jeux-video/src/chlorine/scene/componentManager.hpp
+++ jeux-video/src/chlorine/scene/componentManager.hpp
@@ -18,18 +18,35 @@
 
 namespace chlorine::scene
 {
-    template<typename... typesIn>
-    class orchestrator
+    // Just to access stuff
+    // I literally copy pasted this code
+    // God knows how it works
+    template<typename... Ts>
+    struct variantWrapper
     {
-    public:
-        std::unordered_map<std::string, std::unordered_map<std::string, std::variant<std::monostate, typesIn...>>> tempMap;
-
-        template<typename typeIn>
-        bool addStructToMap(typeIn valueIn, std::string key1, std::string key2)
-        {
-            tempMap<typeIn>[key1][key2] = valueIn;
-            return true;
-        }
-    private:
+        std::variant<Ts...> & var;
+        template<typename T>
+        operator T() {return std::get<T>(var);}
     };
+
+    template<typename... typesIn>
+    using instrument = std::variant<typesIn...>;
+
+    using instrumentId = decltype(std::declval<std::type_info>().hash_code());
+
+
+    template<typename... typesIn>
+    class orchestra
+    {
+        std::unordered_map<std::string, std::unordered_map<std::string, instrument<typesIn...>>> instruments;
+
+        // honestly insert/set are the same thing :P
+        // This will be fixed laterrr
+        // switch to const char* instead, or hashes
+        void insertElement(std::string key1, std::string key2, instrument<typesIn...> const& instrumentIn)
+        {
+            instruments[key1][key2] = instrumentIn;
+        }
+    }
+
 }
