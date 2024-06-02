747067877aa45c54a967d43cf402bb6b4cd99e0a
Yohwllo <mp2702737@gmail.com>
2024-06-02 02:22:17 -0400

    Deep down idk if I can do this

---
+++ jeux-video/src/chlorine/scene/component.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace chlorine::scene
+{
+    // this class is LITERALLY a place holder
+    class component
+    {
+    public:
+        virtual void init();
+        virtual void update();
+    };
+}
