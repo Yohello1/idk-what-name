20e7a9633d2e005a4814a08b15ab8e77fb2791c9
Yohwllo <mp2702737@gmail.com>
2024-06-03 21:02:52 -0400

    cut my hair

--- jeux-video/src/chlorine/scene/componentManager.hpp
+++ jeux-video/src/chlorine/scene/componentManager.hpp
@@ -19,28 +19,15 @@
 #include <chlorine/scene/component.hpp>
 namespace chlorine::scene
 {
-    // Just to access stuff
-    // I literally copy pasted this code
-    // God knows how it works
-
-
-
     class orchestra
     {
     public:
         std::unordered_map<std::string, std::unordered_map<std::type_index, std::unique_ptr<::chlorine::scene::component>>> instruments;
 
         template<typename T, typename... Args>
-        void insertElement(std::string key1, Args&&... args)
-        {
-
-        }
-
+        void insertElement(std::string key1, Args&&... args);
         // find of hash next
-        void removeElement(std::string key1, std::type_index key2)
-        {
-            instruments[key1].erase(key2);
-        }
+        void removeElement(std::string key1, std::type_index key2);
 
     private:
         std::unordered_map<std::type_index, std::vector<std::string>> _groups; // I do NOT want this to be modified directly
