416f1f09e4f1818c9a4dcba3dcc2e3d9a67670f7
Yohwllo <mp2702737@gmail.com>
2024-06-08 12:41:38 -0400

    oh sorry, I forgot, I only exist when you need something

--- jeux-video/sanity.sh
+++ jeux-video/sanity.sh
@@ -9,3 +9,11 @@ ninja
 ninja test
 ninja test_memcheck
 
+echo "python
+import sys
+sys.path.insert(0, '/usr/share/gcc-8.2.1/python/')
+# This would only enable the printers but not the xmethods:
+# from libstdcxx.v6.printers import register_libstdcxx_printers
+from libstdcxx.v6 import register_libstdcxx_printers
+register_libstdcxx_printers (None)
+end " > .gdbinit
