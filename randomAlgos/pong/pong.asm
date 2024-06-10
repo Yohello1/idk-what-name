b321f7043612845c909733f5428b01e867badf5f
Yohwllo <mp2702737@gmail.com>
2024-06-10 01:56:54 -0400

    Do the one thing I can do right, push people away

--- randomAlgos/pong/pong.asm
+++ randomAlgos/pong/pong.asm
@@ -1,16 +1,33 @@
 section .data
-    msg db "couple", 0ax
+    msg db "Hello world"
+    lin db 0xa
     len equ $ -msg
 
 section .text
 global _start
 
+
+
 _start:
-    mov edx, len
-    mov ecx, msg
-    mov ebx, 1
+    mov ebx, 1                  ; Set file descriptor to 1
+    mov esi, 0                   ; Set ah to 0
     mov eax, 4
 
+loop1:
+    add esi, 1
+
+    mov edx, esi
+    mov ecx, msg
+    mov eax, 4
     int 0x80
-    mov eax, 1
+
+    mov edx, 1
+    mov ecx, lin
+    mov eax, 4
     int 0x80
+
+    cmp esi, len
+    jle loop1
+
+    mov eax, 1                  ; Tell we're gonna exit
+    int 0x80                    ; Ask it to fufil our request
