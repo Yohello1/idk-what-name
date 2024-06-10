ccb0d4b3e98428c5c3f595e4aed03a0b279ec830
Yohwllo <mp2702737@gmail.com>
2024-06-10 15:04:33 -0400

    never worth the day lights time

--- randomAlgos/pong/pong.asm
+++ randomAlgos/pong/pong.asm
@@ -1,33 +1,16 @@
-section .data
-    msg db "Hello world"
-    lin db 0xa
-    len equ $ -msg
+    global    _start
 
-section .text
-global _start
+    section .data
+hello:
+    db "Hello, World", 10
 
-
-
+    section   .text
 _start:
-    mov ebx, 1                  ; Set file descriptor to 1
-    mov esi, 0                   ; Set ah to 0
-    mov eax, 4
-
-loop1:
-    add esi, 1
-
-    mov edx, esi
-    mov ecx, msg
-    mov eax, 4
-    int 0x80
-
-    mov edx, 1
-    mov ecx, lin
-    mov eax, 4
-    int 0x80
-
-    cmp esi, len
-    jle loop1
-
-    mov eax, 1                  ; Tell we're gonna exit
-    int 0x80                    ; Ask it to fufil our request
+    mov       rax, 1
+    mov       rdi, 1
+    mov       rsi, hello
+    mov       rdx, 13
+    syscall
+    mov       rax, 60
+    xor       rdi, rdi
+    syscall
