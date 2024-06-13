4001fe177ab9dea9552b84042b183364eb462e57
Yohwllo <mp2702737@gmail.com>
2024-06-13 17:14:46 -0400

    were they colluding

--- jeux-video/src/chlorine/io/io.cpp
+++ jeux-video/src/chlorine/io/io.cpp
@@ -4,6 +4,31 @@
 
 namespace chlorine::io
 {
+    bool componentImport(std::string componentPath, std::unique_ptr<::chlorine::scene::scene> const& sceneIn, std::map<std::string, std::function<const std::type_info&(std::unique_ptr<::chlorine::scene::component>&)>> mapSwitcher, std::unique_ptr<::chlorine::logging::logBase> const &logOut)
+    {
+        std::fstream componentFile;
+        componentFile.open(componentPath, std::ios::in);
+        if(componentFile.is_open() == false)
+        {
+            logOut->log("Could not open file" + componentPath);
+            return false;
+        }
+
+        std::string componentFileLine, componentName;
+        std::getline(componentFile, componentFileLine);
+        std::vector<std::string> componentFileSplit;
+        ::chlorine::utils::splitStringToVector(componentFileLine, componentFileSplit, ' ');
+        std::getline(componentFile, componentName);
+
+        componentFile.close();
+
+        std::unique_ptr<::chlorine::scene::component> tempPtr;
+        const std::type_info& tempTypeInfo = mapSwitcher[componentFileSplit[1]](tempPtr);
+        tempPtr->loadFile(componentPath);
+        sceneIn->Conductor.instruments[componentName].emplace(tempTypeInfo, std::move(tempPtr));
+
+        return true;
+    }
 
     bool sceneImport(std::unique_ptr<::chlorine::scene::scene> const& sceneIn, std::string pathPrefix, std::string filePath, std::map<std::string, std::function<const std::type_info&(std::unique_ptr<::chlorine::scene::component>&)>> mapSwitcher, std::unique_ptr<::chlorine::logging::logBase> const &logOut)
     {
@@ -32,39 +57,18 @@ namespace chlorine::io
         {
             std::vector<std::string> tempSplit;
             ::chlorine::utils::splitStringToVector(nextLine, tempSplit, ' ' );
-            std::string componentPath = pathPrefix + sceneIn->sceneName + "/" + tempSplit[1];
             if(tempSplit[0] == "component")
             {
-                // sceneIn->components.insert(std::make_pair(tempSplit[1], std::unique_ptr<::chlorine::scene::component>()));
-                // logOut->log("Made component: " + tempSplit[1] + '\n');
-                std::fstream componentFile;
-                componentFile.open(componentPath, std::ios::in);
-                if(componentFile.is_open() == false)
-                {
-                    logOut->log("Cold not open component file: " + componentPath);
-                }
-
-                std::string componentFileLine, componentName;
-                std::getline(componentFile, componentFileLine);
-                std::vector<std::string> componentFileSplit;
-                ::chlorine::utils::splitStringToVector(componentFileLine, componentFileSplit, ' '); // 0 = "type", 1 = the actual type name
-                std::getline(componentFile, componentName);
-
-                componentFile.close();
-
-                std::unique_ptr<::chlorine::scene::component> tempPtr;
-                const std::type_info& tempTypeInfo = mapSwitcher[componentFileSplit[1]](tempPtr);
-                // Load file function
-                sceneIn->Conductor.instruments[componentName].emplace(tempTypeInfo, std::move(tempPtr));
-
+                std::string componentPath = pathPrefix + sceneIn->sceneName + "/" + tempSplit[1];
+                componentImport(componentPath, sceneIn, mapSwitcher, logOut);
             }
             else if(tempSplit[0] == "xpos")
             {
-                sceneIn->windowSize.first = std::stoi(tempSplit[1]);
+                sceneIn->windowSize.first = 10;
             }
             else if(tempSplit[0] == "ypos")
             {
-                sceneIn->windowSize.first = std::stoi(tempSplit[1]);
+                sceneIn->windowSize.second = 20;
             }
         }
 
