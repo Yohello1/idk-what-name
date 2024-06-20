fab416999a17ef288c0fe4407fbdf09b1fadb9e0
Yohwllo <mp2702737@gmail.com>
2024-06-20 15:55:35 -0400

    commit to them all

---
+++ jeux-video/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/yohello/code/generation/jeux-video
+# Build directory: /home/yohello/code/generation/jeux-video/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(example_test "posionedchlorine")
+set_tests_properties(example_test PROPERTIES  _BACKTRACE_TRIPLES "/home/yohello/code/generation/jeux-video/CMakeLists.txt;20;add_test;/home/yohello/code/generation/jeux-video/CMakeLists.txt;0;")
