2e331497290d71975ff840bb92c8fcebec2da6cb
Yohwllo <mp2702737@gmail.com>
2024-06-20 20:41:14 -0400

    Finished intersection sphere question

--- randomAlgos/ship/intersection/solver.cpp
+++ randomAlgos/ship/intersection/solver.cpp
@@ -2,19 +2,8 @@
 #include <vector>
 #include <glm/glm.hpp>
 
-std::vector<std::pair<glm::vec3, std::pair<glm::vec3, glm::vec3>>> trianglePoints;
-std::vector<std::pair<glm::vec3, glm::vec3>> vectors;
-std::vector<glm::vec3> normals;
-
-
 int main()
 {
-    struct Ray
-    {
-        glm::vec3 startPoint;
-        glm::vec3 rayDirection;
-        float t;
-    };
 
 
 
