d55f2a5e5e83451c306c2418f0df82338d083159
Yohwllo <mp2702737@gmail.com>
2024-07-12 23:36:48 -0400

    Motion blur

--- raylith/src/main.cpp
+++ raylith/src/main.cpp
@@ -9,13 +9,15 @@
 #include <iostream>
 
 int main() {
+
+    srand(time(NULL));
     hittable_list world;
 
     auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
     world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
 
-    for (int a = -11; a < 11; a++) {
-        for (int b = -11; b < 11; b++) {
+    for (int a = -12; a < 12; a++) {
+        for (int b = -12; b < 12; b++) {
             auto choose_mat = random_double();
             point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
 
@@ -26,7 +28,8 @@ int main() {
                     // diffuse
                     auto albedo = color::random() * color::random();
                     sphere_material = make_shared<lambertian>(albedo);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                    auto center2 = center + vec3(0, random_double(0,.5), 0);
+                    world.add(make_shared<sphere>(center, center2, 0.2, sphere_material));
                 } else if (choose_mat < 0.95) {
                     // metal
                     auto albedo = color::random(0.5, 1);
@@ -42,22 +45,21 @@ int main() {
         }
     }
 
-    auto material1 = make_shared<dielectric>(1.5);
+    auto material1 = make_shared<dielectric>(1.3);
     world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
 
-    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
-    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+    auto material2 = make_shared<lambertian>(color(0.439, 0.7647, 0.9137));
+    world.add(make_shared<sphere>(point3(-4, 1, 0), 0.9, material2));
 
-    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+    auto material3 = make_shared<metal>(color(1.0, 0.2, 1.0), 0.0);
     world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
 
     camera cam;
 
-    cam.aspect_ratio      = 16.0 / 10.0;
-    cam.image_width       = 1920;
-    cam.samples_per_pixel = 50;
+    cam.aspect_ratio      = 16.0 / 9.0;
+    cam.image_width       = 400;
+    cam.samples_per_pixel = 100;
     cam.max_depth         = 50;
-
     cam.vfov     = 20;
     cam.lookfrom = point3(13,2,3);
     cam.lookat   = point3(0,0,0);
