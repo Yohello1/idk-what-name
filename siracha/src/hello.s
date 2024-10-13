eadb7946a06514d934cf7078525be689982849c6
Yohwllo <mp2702737@gmail.com>
2024-10-13 01:23:28 -0400

    please dont commit suicide

--- siracha/src/hello.s
+++ siracha/src/hello.s
@@ -2,6 +2,7 @@
 .section .text.bios
 
 _start:
+loop:
     addi a0, x0, 0x68
     li a1, 0x10000000
     sb a0, (a1)                 ;
@@ -27,5 +28,4 @@ _start:
     add a4, a2, a3
     sb a4, (a1)
 
-loop:
     j loop
