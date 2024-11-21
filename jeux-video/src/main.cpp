4d95d00fa195f3b4270b94f747d30d43efa5483c
Yohwllo <mp2702737@gmail.com>
2024-11-20 21:06:09 -0500

    Setup compute

--- jeux-video/src/main.cpp
+++ jeux-video/src/main.cpp
@@ -13,138 +13,9 @@
 #include <chlorine/scene/component.hpp>
 #include <chlorine/scene/componentManager.hpp>
 
-
-class boxes : public chlorine::scene::component
+std::map<std::string, std::function<std::type_index(std::string, chlorine::scene::orchestra&)>> tempMap
 {
-public:
-    std::pair<std::uint32_t, std::uint32_t> position;
 
-    bool loadFile (const std::string &filePath, std::unique_ptr<::chlorine::logging::logBase> const &logOut) override
-    {
-        (void)logOut;
-
-
-        std::fstream componentFile;
-        componentFile.open(filePath);
-
-        std::string line;
-        std::uint32_t lineNumber = 0;
-
-        while(std::getline(componentFile, line))
-        {
-            lineNumber++;
-            if(lineNumber == 2)
-                name = line;
-            else if(lineNumber == 3)
-            {
-                std::vector<std::string> temppString;
-                chlorine::utils::splitStringToVector(line, temppString, ' ');
-                position.first = std::stoi(temppString[1]);
-            }
-            else if(lineNumber == 4)
-            {
-                std::vector<std::string> temppString;
-                chlorine::utils::splitStringToVector(line, temppString, ' ');
-                position.first = std::stoi(temppString[1]);
-            }
-        }
-
-        return true;
-    }
-
-    bool saveFile(const std::string &filePath, std::unique_ptr<::chlorine::logging::logBase> const &logOut) override
-    {
-        (void)logOut;
-        std::ofstream componentFile;
-        componentFile.open(filePath);
-        componentFile << "type " << chlorine::io::_stringTypeConv[std::type_index(typeid(*this))] << '\n';
-        componentFile << name << '\n';
-        componentFile << "xpos " << position.first << '\n';
-        componentFile << "ypos " << position.second << '\n';
-        componentFile.close();
-        return true;
-    }
-
-
-	virtual ~boxes() = default;
-};
-
-class windowX : public chlorine::scene::component
-{
-public:
-    std::uint16_t windowX;
-    bool loadFile(const std::string &filePath, std::unique_ptr<::chlorine::logging::logBase> const &logOut) override
-    {
-        (void)logOut;
-        std::fstream componentFile;
-        componentFile.open(filePath);
-
-        std::string line;
-        std::uint32_t lineNumber = 0;
-
-        while(std::getline(componentFile, line))
-        {
-            lineNumber++;
-            if(lineNumber == 2)
-            {
-                name = line;
-            }
-            else if(lineNumber == 3)
-            {
-                std::vector<std::string> tempString;
-                chlorine::utils::splitStringToVector(line, tempString, ' ');
-                windowX = std::stoi(tempString[1]);
-            }
-        }
-        return true;
-    }
-
-};
-
-class windowY : public chlorine::scene::component
-{
-public:
-    std::uint16_t windowY;
-    bool loadFile(const std::string &filePath, std::unique_ptr<::chlorine::logging::logBase> const &logOut) override
-    {
-        std::fstream componentFile;
-        componentFile.open(filePath);
-
-        std::string line;
-        std::uint32_t lineNumber = 0;
-
-        while(std::getline(componentFile, line))
-        {
-            lineNumber++;
-            if(lineNumber == 2)
-            {
-                name = line;
-            }
-            else if(lineNumber == 3)
-            {
-                std::vector<std::string> tempString;
-                chlorine::utils::splitStringToVector(line, tempString, ' ');
-                windowY = std::stoi(tempString[1]);
-            }
-        }
-        return true;
-    }
-};
-
-std::map<std::string, std::function<std::type_index(std::string, chlorine::scene::orchestra&)>> tempMap3
-{
-    {"boxes", [](std::string stringIn, chlorine::scene::orchestra& Conductor) -> std::type_index{
-        Conductor.instruments[stringIn].emplace(std::type_index(typeid(boxes)), std::make_unique<boxes>());
-        return std::type_index(typeid(boxes));}
-    },
-    {"windowX", [](std::string stringIn, chlorine::scene::orchestra& Conductor) -> std::type_index{
-        Conductor.instruments[stringIn].emplace(std::type_index(typeid(windowX)), std::make_unique<windowX>());
-        return std::type_index(typeid(windowX));}
-    },
-    {"windowY", [](std::string stringIn, chlorine::scene::orchestra& Conductor) -> std::type_index{
-        Conductor.instruments[stringIn].emplace(std::type_index(typeid(windowY)), std::make_unique<windowY>());
-        return std::type_index(typeid(windowY));}
-    },
 };
 
 
@@ -157,7 +28,7 @@ int main()
     std::unique_ptr<chlorine::scene::scene> tempScene = std::make_unique<::chlorine::scene::scene>(logOut);
     logOut->log("hello\n");
 
-    chlorine::io::sceneImport(tempScene, "../test/" ,"../test/Arrowhead.pcsf", tempMap3, logOut);
+    chlorine::io::sceneImport(tempScene, "../test/" ,"../test/Arrowhead.pcsf", tempMap, logOut);
     tempScene->sceneName = "timeSpent";
     chlorine::io::sceneExport(tempScene, "../test/", logOut);
 
