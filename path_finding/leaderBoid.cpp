862ba7d5d5872bd527c385b4c7abc64b2faff82a
Yohwllo <mp2702737@gmail.com>
2024-12-25 03:33:34 -0500

    just let me pretend

--- path_finding/leaderBoid.cpp
+++ path_finding/leaderBoid.cpp
@@ -10,7 +10,7 @@
 #include <unistd.h>
 
 #define MAP_SIZE 128
-#define BIRD_AMT 800
+#define BIRD_AMT 8000
 
 class bird;
 sf::RenderWindow window(sf::VideoMode(1024,1024), "SFML works!");
@@ -18,11 +18,8 @@ sf::Texture texture;
 sf::Sprite unvisted, visited, start, end, path, wall;
 
 float maxSpeed = 4, minSpeed = 3;
-double centeringFactor = 0.5;
 double steeringFactor = 0.05;
-double matchingFactor = 1;
 double turnFactor = 5;
-double avoidanceFactor = 0.4;
 
 std::array<std::array<int, 128>, 128> map;
 std::pair<int, int> cursor;
@@ -32,106 +29,6 @@ class bird
     public:
     std::pair<float, float> cord;
     std::pair<float, float> velocity;
-    std::vector<int> birdsToEval;
-
-
-    void birdsInRange(std::array<bird, BIRD_AMT> boids, int range)
-    {
-        while(!birdsToEval.empty())
-        {
-            birdsToEval.clear();
-        }
-
-        for(int i = 0; i < BIRD_AMT; i++)
-        {
-            double distance = ((std::pow(std::abs(cord.first - boids[i].cord.first), 2)) +  (std::pow(std::abs(cord.second - boids[i].cord.second), 2)));
-            if(distance < range)
-            {
-                // std::cout << "Bordy in range uwu" << " " << i << std::endl;
-                birdsToEval.push_back(i);
-            }
-        }
-    }
-
-    void cohesionBirds(std::array<bird, BIRD_AMT> boids)
-    {
-        float xAvgPos = 0, xDist;
-        float yAvgPos = 0, yDist;
-
-
-        for(int i = 0; (uint) i < birdsToEval.size(); i++)
-        {
-            xAvgPos += boids[birdsToEval.at(i)].cord.first;
-            yAvgPos += boids[birdsToEval.at(i)].cord.second;
-        }
-
-        if(xAvgPos > 0)
-        {
-            xAvgPos = xAvgPos / birdsToEval.size() - 1;
-            xDist = xAvgPos - cord.first;
-            velocity.first  += xDist*centeringFactor;
-        }
-
-        if(yAvgPos > 0)
-        {
-            yAvgPos = yAvgPos / birdsToEval.size() - 1;
-            yDist = yAvgPos - cord.second;;
-            velocity.second += yDist*centeringFactor;
-        }
-
-    }
-
-    void seperationBirds(std::array<bird, BIRD_AMT> boids)
-    {
-        // first find birds within X radius
-        birdsInRange(boids, 100);
-
-        float xAvgPos = 0, xDist = 0;
-        float yAvgPos = 0, yDist = 0;
-
-        for(int i = 0; (uint) i < birdsToEval.size(); i++)
-        {
-            xAvgPos += boids[birdsToEval.at(i)].cord.first;
-            yAvgPos += boids[birdsToEval.at(i)].cord.second;
-        }
-
-        if(xAvgPos > 0)
-        {
-            xAvgPos = xAvgPos / birdsToEval.size();
-            xDist = xAvgPos - cord.first;
-            velocity.first  += xDist*avoidanceFactor*-1;
-        }
-
-        if(yAvgPos > 0)
-        {
-            yAvgPos = yAvgPos / birdsToEval.size();
-            yDist = yAvgPos - cord.second;
-            velocity.second += yDist*avoidanceFactor*-1;
-        }
-    }
-
-    void alignBirds(std::array<bird, BIRD_AMT> boids)
-    {
-        // get avg velocity
-        float xVelAvg = 0;
-        float yVelAvg = 0;
-
-        for(int i = 0; (uint) i < birdsToEval.size(); i++)
-        {
-            xVelAvg += boids[birdsToEval.at(i)].velocity.first;
-            yVelAvg += boids[birdsToEval.at(i)].velocity.second;
-        }
-
-        if(birdsToEval.size() > 0)
-        {
-            xVelAvg = xVelAvg / birdsToEval.size();
-            yVelAvg = yVelAvg / birdsToEval.size();
-            velocity.first += (xVelAvg)*matchingFactor;
-            velocity.second += (yVelAvg)*matchingFactor;
-        }
-
-
-    }
 
     void screenEdges()
     {
@@ -166,20 +63,8 @@ class bird
         {
             cord.second += velocity.second;
         }
-
-        // velocity.first = 0;
-        // velocity.second = 0;
-
-        // birdsInRange(boids, 500);
-
-        // cohesionBirds(boids);
-        // alignBirds(boids);
         seekLeader(cursor);
-        //collisionAvoidance();
         screenEdges();
-
-        // NOTE: SEPERATION MUST BE RUN AT THE END!!!
-        // seperationBirds(boids);
 
         float speed = std::sqrt(std::pow(velocity.first,2) + std::pow(velocity.second,2));
         if(speed > maxSpeed)
@@ -257,7 +142,7 @@ int main()
 
         for(int i = 0; i < BIRD_AMT; i++)
         {
-            boids[i].birdsInRange(boids, BIRD_AMT);
+            // boids[i].birdsInRange(boids, BIRD_AMT);
             boids[i].velocity = {rand() % 2, rand() % 2};
         }
 
