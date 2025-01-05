4ee1888e6bd3056a8e75d3e590801165c4cb88eb
Yohwllo <mp2702737@gmail.com>
2025-01-05 14:24:45 -0500

    cause ur biggest fear is that, someone will leave

--- jeux-video/src/main.cpp
+++ jeux-video/src/main.cpp
@@ -6,6 +6,9 @@
 #include <functional>
 #include <typeindex>
 
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+
 #include <chlorine/logs/logs.hpp>
 #include <chlorine/utils/strings.hpp>
 #include <chlorine/scene/scene.hpp>
@@ -69,105 +72,13 @@ public:
 	virtual ~boxes() = default;
 };
 
-class windowX : public chlorine::scene::component
-{
-public:
-    std::uint16_t windowX;
-    bool loadFile(const std::string &filePath, std::unique_ptr<::chlorine::logging::logBase> const &logOut) override
-    {
-        (void)logOut;
-        std::fstream componentFile;
-        componentFile.open(filePath);
-
-        std::string line;
-        std::uint32_t lineNumber = 0;
-
-        while(std::getline(componentFile, line))
-        {
-            lineNumber++;
-            if(lineNumber == 2)
-            {
-                name = line;
-            }
-            else if(lineNumber == 3)
-            {
-                std::vector<std::string> tempString;
-                chlorine::utils::splitStringToVector(line, tempString, ' ');
-                windowX = std::stoi(tempString[1]);
-            }
-        }
-        return true;
-    }
-
-    bool saveFile(const std::string &filePath, std::unique_ptr<::chlorine::logging::logBase> const &logOut) override
-    {
-        (void)logOut;
-        std::ofstream componentFile;
-        componentFile.open(filePath);
-        componentFile << "type " << chlorine::io::_stringTypeConv[std::type_index(typeid(*this))] << '\n';
-        componentFile << name << '\n';
-        componentFile << name << ' ' << windowX << '\n';
-        componentFile.close();
-        return true;
-    }
-};
-
-class windowY : public chlorine::scene::component
-{
-public:
-    std::uint16_t windowY;
-    bool loadFile(const std::string &filePath, std::unique_ptr<::chlorine::logging::logBase> const &logOut) override
-    {
-        std::fstream componentFile;
-        componentFile.open(filePath);
-
-        std::string line;
-        std::uint32_t lineNumber = 0;
-
-        while(std::getline(componentFile, line))
-        {
-            lineNumber++;
-            if(lineNumber == 2)
-            {
-                name = line;
-            }
-            else if(lineNumber == 3)
-            {
-                std::vector<std::string> tempString;
-                chlorine::utils::splitStringToVector(line, tempString, ' ');
-                windowY = std::stoi(tempString[1]);
-            }
-        }
-        return true;
-    }
-
-    bool saveFile(const std::string &filePath, std::unique_ptr<::chlorine::logging::logBase> const &logOut) override
-    {
-        (void)logOut;
-        std::ofstream componentFile;
-        componentFile.open(filePath);
-        componentFile << "type " << chlorine::io::_stringTypeConv[std::type_index(typeid(*this))] << '\n';
-        componentFile << name << '\n';
-        componentFile << name << ' ' << windowY << '\n';
-        componentFile.close();
-        return true;
-    }
-};
-
 std::map<std::string, std::function<std::type_index(std::string, chlorine::scene::orchestra&)>> tempMap3
 {
     {"boxes", [](std::string stringIn, chlorine::scene::orchestra& Conductor) -> std::type_index{
         Conductor.instruments[stringIn].emplace(std::type_index(typeid(boxes)), std::make_unique<boxes>());
         return std::type_index(typeid(boxes));}
-    },
-    {"windowX", [](std::string stringIn, chlorine::scene::orchestra& Conductor) -> std::type_index{
-        Conductor.instruments[stringIn].emplace(std::type_index(typeid(windowX)), std::make_unique<windowX>());
-        return std::type_index(typeid(windowX));}
-    },
-    {"windowY", [](std::string stringIn, chlorine::scene::orchestra& Conductor) -> std::type_index{
-        Conductor.instruments[stringIn].emplace(std::type_index(typeid(windowY)), std::make_unique<windowY>());
-        return std::type_index(typeid(windowY));}
-    },
+    }
+
 };
 
 
