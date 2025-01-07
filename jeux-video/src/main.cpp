82e930abb8a1749689669a458d5b55c8fa7d44af
Yohwllo <mp2702737@gmail.com>
2025-01-06 22:24:57 -0500

    walk

--- jeux-video/src/main.cpp
+++ jeux-video/src/main.cpp
@@ -6,8 +6,8 @@
 #include <functional>
 #include <typeindex>
 
-// #include <glad/glad.h>
-// #include <GLFW/glfw3.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 #include <chlorine/logs/logs.hpp>
 #include <chlorine/utils/strings.hpp>
@@ -15,6 +15,11 @@
 #include <chlorine/io/io.hpp>
 #include <chlorine/scene/component.hpp>
 #include <chlorine/scene/componentManager.hpp>
+
+void endItAll()
+{
+    // kill everything I think?
+}
 
 
 class boxes : public chlorine::scene::component
@@ -72,10 +77,51 @@ public:
 	virtual ~boxes() = default;
 };
 
-class boxies : public chlorine::scene::component
+class window_container : public chlorine::scene::component
 {
 public:
-    std::pair<std::uint32_t, std::uint32_t> position;
+    GLFWwindow *window;
+
+    uint16_t width, height;
+
+    window_container(std::string string_in)
+    {
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+        glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+        window = glfwCreateWindow(width, height, "draw2", NULL, NULL);
+        if (!window)
+        {
+            glfwTerminate();
+            exit(EXIT_FAILURE);
+        }
+
+        std::vector<std::string> splitVector;
+
+        ::chlorine::utils::splitStringToVector(string_in, &splitVector, ' ');
+
+    }
+
+    void update()
+    {
+        glViewport(0, 0, width, height);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+    }
+
+    void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+    {
+        if (key == GLFW_KEY_Q && action == GLFW_PRESS)
+            endItAll();
+    }
 
     bool loadFile (const std::string &filePath, std::unique_ptr<::chlorine::logging::logBase> const &logOut) override
     {
@@ -92,7 +138,7 @@ public:
     }
 
 
-	virtual ~boxies() = default;
+	virtual ~window_container() = default;
 };
 
 
@@ -101,7 +147,13 @@ std::map<std::string, std::function<std::type_index(std::string, chlorine::scene
     {"boxes", [](std::string stringIn, chlorine::scene::orchestra& Conductor) -> std::type_index{
         Conductor.instruments[stringIn].emplace(std::type_index(typeid(boxes)), std::make_unique<boxes>());
         return std::type_index(typeid(boxes));}
+    },
+    {"window_container", [](std::string stringIn, chlorine::scene::orchestra& Conductor) -> std::type_index{
+        Conductor.instruments[stringIn].emplace(std::type_index(typeid(window_container)),
+                                                std::make_unique<window_container>());
+        return std::type_index(typeid(window_container));}
     }
+
 
 };
 
